1016fd090bc215a58ce69450e52a15200f783034
Jacob Hoopes <jake.hoopes@gmail.com>
2016-08-02 13:31:24 -0600

    Vagrant dnsrecon.sh - install dependencies using pip

--- vagrant/install/dnsrecon.sh
+++ vagrant/install/dnsrecon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 printf "Installing DNSRecon *******************************************************************"
 dnsrecon_root="/opt/dnsrecon-git"
-sudo -i /bin/bash --login -c "git clone https://github.com/darkoperator/dnsrecon.git ${dnsrecon_root}"
+sudo -i /bin/bash --login -c "git clone https://github.com/darkoperator/dnsrecon.git ${dnsrecon_root} && cd ${dnsrecon_root} && pip install -r requirements.txt"
