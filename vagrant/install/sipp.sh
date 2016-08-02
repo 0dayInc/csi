44bed4c5961223629c26874493cf0d2f97fdf829
Jacob Hoopes <jake.hoopes@gmail.com>
2016-08-02 10:16:22 -0600

    Vagrant sipp.sh - add libgsl-dev package dependency

--- vagrant/install/sipp.sh
+++ vagrant/install/sipp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 printf "Installing SIPp ***********************************************************************"
 sipp_root="/opt/sipp-git"
-sudo -i /bin/bash --login -c "apt-get install -y libsctp-dev && git clone https://github.com/sipp/sipp ${sipp_root} && cd ${sipp_root} && ./build.sh --full && cp ${sipp_root}/sipp /usr/local/bin"
+sudo -i /bin/bash --login -c "apt-get install -y libsctp-dev libgsl-dev && git clone https://github.com/sipp/sipp ${sipp_root} && cd ${sipp_root} && ./build.sh --full && cp ${sipp_root}/sipp /usr/local/bin"
