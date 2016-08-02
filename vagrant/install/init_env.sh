926a75f2bf8973a4083b267cc8ba87b70e19c08d
Jacob Hoopes <jake.hoopes@gmail.com>
2016-08-02 12:40:20 -0600

    Vagrantfile & init_env.sh - initialize aws & virtualbox to behave during installation

--- vagrant/install/init_env.sh
+++ vagrant/install/init_env.sh
@@ -4,4 +4,4 @@ echo "Updating FQDN: ${hostname}"
 cat /etc/hosts | grep "${hostname}" || sudo sed "s/127.0.0.1/127.0.0.1 ${hostname}/g" -i /etc/hosts
 hostname | grep "${hostname}" || sudo hostname "${hostname}"
 sudo sed -i -e 's/^Defaults.*requiretty/# Defaults requiretty/g' /etc/sudoers
-sudo echo 'Defaults:ubuntu !requiretty' >> /etc/sudoers
+sudo /bin/bash --login -c 'echo "Defaults:ubuntu !requiretty" >> /etc/sudoers'
