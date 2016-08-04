8ce8aee716675034a9a0190e4969d7b2bec44d1f
Jacob Hoopes <jake.hoopes@gmail.com>
2016-08-04 11:09:52 -0600

    Vagrant lxde.sh - change default wallpaper

--- vagrant/install/lxde.sh
+++ vagrant/install/lxde.sh
@@ -1,3 +1,3 @@
 #!/bin/bash --login
 printf "Installing LXDE ***********************************************************************"
-sudo /bin/bash --login -c "apt-get install -y lxde lxdm && /etc/init.d/lxdm restart"
+sudo /bin/bash --login -c "apt-get install -y lxde lxdm && /etc/init.d/lxdm restart && ln -sf /csi/virtualbox-gui_wallpaper.jpg /etc/alternatives/desktop-background"
