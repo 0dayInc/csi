7815ee475b0c46ef12badf6a781180c1b73232a9
Jacob Hoopes <jake.hoopes@gmail.com>
2016-08-03 18:54:39 -0600

    Vagrant tesseract.sh - minor bugfix

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,3 +1,3 @@
 module CSI
-  VERSION = '0.1.130'
+  VERSION = '0.1.131'
 end
--- vagrant/install/tesseract.sh
+++ vagrant/install/tesseract.sh
@@ -1,4 +1,3 @@
 #!/bin/bash --login
 printf "Installing Tesseract OCR **************************************************************"
 sudo apt-get install -y tesseract-ocr-all
-sudo /etc/init.d/lxdm restart
