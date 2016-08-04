4a428df23c94a4499df6703b605602565ae165b7
Jacob Hoopes <jake.hoopes@gmail.com>
2016-08-04 11:36:45 -0600

    Vagrant lxde.sh - change default wallpaper

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,3 +1,3 @@
 module CSI
-  VERSION = '0.1.135'
+  VERSION = '0.1.136'
 end
--- vagrant/install/lxde.sh
+++ vagrant/install/lxde.sh
@@ -1,3 +1,3 @@
 #!/bin/bash --login
 printf "Installing LXDE ***********************************************************************"
-sudo /bin/bash --login -c "apt-get install -y lxde lxdm && /etc/init.d/lxdm restart && ln -sf /csi/virtualbox-gui_wallpaper.jpg /etc/alternatives/desktop-background"
+sudo /bin/bash --login -c "apt-get install -y lxde lxdm && sed -i 's/bg=\/usr\/share\/images\/desktop-base\/login-background\.svg/bg=\/etc\/alternatives\/desktop\-background/g' /etc/lxdm/default.conf && /etc/init.d/lxdm restart && ln -sf /csi/virtualbox-gui_wallpaper.jpg /etc/alternatives/desktop-background"
