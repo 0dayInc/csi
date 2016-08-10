3bc144a3cb02056ce2dea0522dc3ad87c2e976d9
Jacob Hoopes <jake.hoopes@gmail.com>
2016-08-10 14:47:41 -0600

    Vagrant letsencrypt.sh - disable dialog screen to prevent vagrant up failure

--- vagrant/install/letsencrypt.sh
+++ vagrant/install/letsencrypt.sh
@@ -3,4 +3,4 @@ printf "Installing Let's Encrypt ***********************************************
 letsencrypt_root="/opt/letsencrypt-git"
 domain_name=`hostname -d`
 #TODO: Figure out how to either A. leverage letsencrypt w/ <domain>.local as the domain or B. auto-enroll host w/ AWS Route53 :-/
-sudo -i /bin/bash --login -c "git clone https://github.com/letsencrypt/letsencrypt ${letsencrypt_root} && cd ${letsencrypt_root} && ./letsencrypt-auto --apache -d csi.$domain_name -d openvas.$domain_name -d scapm.$domain_name --register-unsafely-without-email"
+sudo -i /bin/bash --login -c "git clone https://github.com/letsencrypt/letsencrypt ${letsencrypt_root} && cd ${letsencrypt_root} && ./letsencrypt-auto --text --apache -d csi.$domain_name -d openvas.$domain_name -d scapm.$domain_name --register-unsafely-without-email"
