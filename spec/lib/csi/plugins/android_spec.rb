351b21ed8bec92e995e51676311bef928a323063
Jacob Hoopes <jake.hoopes@gmail.com>
2016-08-23 15:15:37 -0600

    CSI::Plugins::Android module - intial commit

--- spec/lib/csi/plugins/android_spec.rb
+++ spec/lib/csi/plugins/android_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
-describe CSI::Plugins::AndroidADB do
+describe CSI::Plugins::Android do
   it "should display information for authors" do
-    authors_response = CSI::Plugins::AndroidADB
+    authors_response = CSI::Plugins::Android
     expect(authors_response).to respond_to :authors
   end
 
   it "should display information for existing help method" do
-    help_response = CSI::Plugins::AndroidADB
+    help_response = CSI::Plugins::Android
     expect(help_response).to respond_to :help
   end
 end
