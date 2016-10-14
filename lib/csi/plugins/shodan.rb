d70fba09129b7ad0d5f310d9b861cc0ee77e1a2f
Jacob Hoopes <jake.hoopes@gmail.com>
2016-10-14 12:16:44 -0600

    CSI::Plugins::Shodan module - minor bugfix in #shodan_rest_call method when dealing with comma-delimited IPs in which some may return HTTP responses other than 200

--- lib/csi/plugins/shodan.rb
+++ lib/csi/plugins/shodan.rb
@@ -99,13 +99,8 @@ module CSI
               )
               services_by_ips.push(JSON.parse(response))
             rescue => e
-              case e.message
-                when"404 Resource Not Found"
-                  next
-              else
-                raise e.message
-                exit
-              end
+              services_by_ips.push(e.message)
+              next
             end
           end
           return services_by_ips
--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,3 +1,3 @@
 module CSI
-  VERSION = '0.1.482'
+  VERSION = '0.1.483'
 end
