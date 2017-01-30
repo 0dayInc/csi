b58fc2f976cbfbda84a5c8afa202e2c1d498f217
Jacob Hoopes <jake.hoopes@gmail.com>
2017-01-30 01:52:09 -0700

    CSI::Plugins::OwaspZapIt - add #spider method, minor bugfix

--- lib/csi/plugins/owasp_zap.rb
+++ lib/csi/plugins/owasp_zap.rb
@@ -26,7 +26,9 @@ module CSI
                       end
         params = opts[:params]
         http_body = opts[:http_body].to_s.scrub
-        base_zap_api_uri = 'http://zap'
+        host = zap_obj[:host]
+        port = zap_obj[:port]
+        base_zap_api_uri = "http://#{host}:#{port}"
 
         begin
           rest_client = CSI::Plugins::TransparentBrowser.open(browser_type: :rest)::Request
@@ -116,7 +118,12 @@ module CSI
             proxy = opts[:proxy].to_s.scrub.strip.chomp
             proxy_uri = URI.parse(proxy)
             owasp_zap_cmd << " -host #{proxy_uri.host} -port #{proxy_uri.port}"
+          else
+            proxy = 'http://127.0.0.1:8080'
+            proxy_uri = URI.parse(proxy)
           end
+          zap_obj[:host] = proxy_uri.host.to_s.scrub
+          zap_obj[:port] = proxy_uri.port.to_i
 
           File.open(@output_path, 'w') do |file|
             PTY.spawn(owasp_zap_cmd) do |stdout, _stdin, pid|
--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,3 +1,3 @@
 module CSI
-  VERSION = '0.1.670'
+  VERSION = '0.1.671'
 end
