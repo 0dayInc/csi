c106a73b2d84ab61ca6340c7e054f96b4c558afb
Jacob Hoopes <jake.hoopes@gmail.com>
2017-02-15 17:11:49 -0700

    RVM - minor bugfixes to ensure csi becomes the default gemset for ruby

--- reinstall_csi_gemset.sh
+++ reinstall_csi_gemset.sh
@@ -7,10 +7,10 @@ rvm use $ruby_version@global
 rvm gemset --force delete $ruby_gemset
 rm Gemfile.lock
 rvm gemset create $ruby_gemset
-rvm --default $ruby_version@$ruby_gemset
 rvm use $ruby_version@$ruby_gemset
 gem install bundler
 if [[ $(uname -s) == "Darwin" ]]; then
   bundle config build.pg --with-pg-config=/opt/local/lib/postgresql96/bin/pg_config
 fi
 bundle install
+rvm --default $ruby_version@$ruby_gemset
