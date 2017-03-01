88ea4fe1cefb8ae080804fdc54de3129e91ee001
Jacob Hoopes <jake.hoopes@gmail.com>
2017-03-01 13:37:41 -0700

    CSI::WWW::AppCobaltIO module - initial commit

--- lib/csi/www.rb
+++ lib/csi/www.rb
@@ -4,7 +4,7 @@ module CSI
   # into memory only when they're needed. For more information, see:
   # http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
   module WWW
-    autoload :AppCobaltIO, 'csi/www/app_cobalt_io',
+    autoload :AppCobaltIO, 'csi/www/app_cobalt_io'
     autoload :Bing, 'csi/www/bing'
     autoload :Checkip, 'csi/www/checkip.rb'
     autoload :Duckduckgo, 'csi/www/duckduckgo'
