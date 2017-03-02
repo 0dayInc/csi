8ff9af18191416fe095701ed836fe195035f41b6
Jacob Hoopes <jake.hoopes@gmail.com>
2017-03-02 12:38:18 -0700

    CSI::WWW::Paypal - rubocop fixes

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 module CSI
-  VERSION = '0.1.915'
+  VERSION = '0.1.916'
 end
--- lib/csi/www/paypal.rb
+++ lib/csi/www/paypal.rb
@@ -84,9 +84,9 @@ module CSI
         browser_obj.button(id: 'btnLogin').wait_until_present.click
 
         if mfa
-          
+          # Send code to SMS
+          browser_obj.button(id: 'btnSelectSoftToken').wait_until_present.click
           until browser_obj.url == 'https://www.paypal.com/myaccount/home'
-            browser_obj.button(id: 'btnSelectSoftToken').wait_until_present.click
             print 'enter mfa token: '
             mfa_token = gets.to_s.scrub.strip.chomp
             browser_obj.text_field(id: 'security-code').wait_until_present.set(mfa_token)
