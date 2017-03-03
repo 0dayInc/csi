349699e9269fd8ce5878bbd98fa84788a16e0614
Jacob Hoopes <jake.hoopes@gmail.com>
2017-03-02 20:47:46 -0700

    Vagrant install/nmap.sh - prefer package manager version over SVN

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 module CSI
-  VERSION = '0.1.930'
+  VERSION = '0.1.931'
 end
--- vagrant/install/nmap.sh
+++ vagrant/install/nmap.sh
@@ -1,4 +1,3 @@
 #!/bin/bash --login
 printf "Installing Nmap ***********************************************************************"
-sudo apt-get install -y subversion hping3 p0f dsniff
-sudo /bin/bash --login -c "cd /opt && svn co https://svn.nmap.org/nmap nmap-dev && cd /opt/nmap-dev && ./configure && make && make install"
+sudo apt-get install -y nmap
