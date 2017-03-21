0f198b770fdd4f795e7eda97da15ad74beb78a34
Jacob Hoopes <jake.hoopes@gmail.com>
2017-03-21 15:05:40 -0600

    CSI::WWW::Paypal - add #signup method

--- lib/csi/www/paypal.rb
+++ lib/csi/www/paypal.rb
@@ -56,9 +56,67 @@ module CSI
       end
 
       # Supported Method Parameters::
+      # browser_obj = CSI::WWW::Paypal.signup(
+      #   browser_obj: 'required - browser_obj returned from #open method',
+      #   first_name: 'required - first name',
+      #   last_name: 'required - last name',
+      #   address: 'required - address',
+      #   city: 'required - city',
+      #   state: 'required - state abbreviation',
+      #   zip_code: 'required - zip code',
+      #   mobile_phone: 'required - mobile phone',
+      #   username: 'required - username (email address)',
+      #   password: 'optional - passwd (will prompt if blank)',
+      # )
+
+      public
+
+      def self.signup(opts = {})
+        browser_obj = opts[:browser_obj]
+        first_name = opts[:first_name].to_s.scrub.strip.chomp
+        last_name = opts[:last_name].to_s.scrub.strip.chomp
+        address = opts[:address].to_s.scrub.strip.chomp
+        city = opts[:city].to_s.scrub.strip.chomp
+        state = opts[:state].to_s.scrub.strip.chomp
+        zip_code = opts[:zip_code].to_s.scrub.strip.chomp
+        mobile_phone = opts[:mobile_phone].to_s.scrub.strip.chomp
+        username = opts[:username].to_s.scrub.strip.chomp
+        password = opts[:password]
+
+        if password.nil?
+          password = CSI::Plugins::AuthenticationHelper.mask_password
+        else
+          password = opts[:password].to_s.scrub.strip.chomp
+        end
+        mfa = opts[:mfa]
+
+        browser_obj.goto('https://www.paypal.com/signup')
+
+        browser_obj.text_field(id: 'email').wait_until_present.set(username)
+        browser_obj.text_field(id: 'password').wait_until_present.set(password)
+        browser_obj.text_field(id: 'confirmPassword').wait_until_present.set(password)
+        browser_obj.button(id: '_eventId_personal').wait_until_present.click
+        browser_obj.text_field(id: 'firstName').wait_until_present.set(first_name)
+        browser_obj.text_field(id: 'lastName').wait_until_present.set(last_name)
+        browser_obj.text_field(id: 'address1').wait_until_present.set(address)
+        browser_obj.text_field(id: 'city').wait_until_present.set(city)
+        browser_obj.select(id: 'state').wait_until_present.set(state)
+        browser_obj.text_field(id: 'postalCode').wait_until_present.set(zip_code)
+        browser_obj.text_field(id: 'phoneNumber').wait_until_present.set(mobile_phone)
+        browser_obj.checkbox(id: 'termsAgree').wait_until_present.click
+        browser_obj.button(id: 'submitBtn').wait_until_present.click
+
+        puts "A confirmation email should be sent to #{username} momentarily..." 
+
+        return browser_obj
+      rescue => e
+        raise e
+      end
+
+      # Supported Method Parameters::
       # browser_obj = CSI::WWW::Paypal.login(
       #   browser_obj: 'required - browser_obj returned from #open method',
-      #   username: 'required - username',
+      #   username: 'required - username (email address)',
       #   password: 'optional - passwd (will prompt if blank)',
       #   mfa: 'optional - if true prompt for mfa token (defaults to false)'
       # )
@@ -156,9 +214,22 @@ module CSI
           )
           puts "browser_obj.public_methods"
 
+          browser_obj = #{self}.signup(
+            browser_obj: 'required - browser_obj returned from #open method',
+            first_name: 'required - first name',
+            last_name: 'required - last name',
+            address: 'required - address',
+            city: 'required - city',
+            state: 'required - state abbreviation',
+            zip_code: 'required - zip code',
+            mobile_phone: 'required - mobile phone',
+            username: 'required - username (email address)',
+            password: 'optional - passwd (will prompt if blank)',
+          )
+
           browser_obj = #{self}.login(
             browser_obj: 'required - browser_obj returned from #open method',
-            username: 'required - username',
+            username: 'required - username (email address)',
             password: 'optional - passwd (will prompt if blank),
             mfa: 'optional - if true prompt for mfa token (defaults to false)'
           )
