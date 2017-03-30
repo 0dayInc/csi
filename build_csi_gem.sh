2f4b19e247d9b72e809e49873a4cf009dc6b0d83
Jacob Hoopes <jake.hoopes@gmail.com>
2017-03-30 13:21:14 -0600

    build_csi_gem.sh - rmove old csi gems in pkg folder

--- build_csi_gem.sh
+++ build_csi_gem.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git pull && rake && rake install && rake rerdoc && gem rdoc --rdoc --ri --overwrite -V csi && echo "Invoking bundle-audit Gemfile Scanner..." && bundle update && bundle-audit update && bundle-audit
+rm pkg/* && git pull && rake && rake install && rake rerdoc && gem rdoc --rdoc --ri --overwrite -V csi && echo "Invoking bundle-audit Gemfile Scanner..." && bundle update && bundle-audit update && bundle-audit
