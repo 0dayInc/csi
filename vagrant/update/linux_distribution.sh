99b2cdd241c5067da66ddf24f27ee4825e2d0ed1
Jacob Hoopes <jake.hoopes@gmail.com>
2017-04-21 00:01:31 -0600

    Vagrant - update/linux_distribution: add full-upgrade for Kali

--- vagrant/update/linux_distribution.sh
+++ vagrant/update/linux_distribution.sh
@@ -6,7 +6,7 @@ printf "Updating ${domain_name} w/ Latest Packages *****************************
 #sudo /bin/bash --login -c "sudo cat /dev/null > /etc/issue && sudo cat /dev/null > /etc/issue.net"
 sudo apt-get update
 sudo apt-get dist-upgrade -y
-sudo apt-get upgrade -y
+sudo apt-get full-upgrade -y
 sudo apt autoremove -y
 sudo apt-get install -y apt-file
 sudo apt-file update
