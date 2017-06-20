a11b7b443dd4dccd6d68777b53601f2db78252e3
Jacob Hoopes <jake.hoopes@gmail.com>
2017-06-20 11:29:31 -0600

    Vagrant - ensure altered configs are _not_ overwritten when calling the linux_distribution.sh script

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.2.102'
+  VERSION = '0.2.103'
 end
--- vagrant/update/linux_distribution.sh
+++ vagrant/update/linux_distribution.sh
@@ -4,9 +4,12 @@ printf "Updating ${domain_name} w/ Latest Packages *****************************
 # Repair "==> default: stdin: is not a tty" message
 #sudo sed -i '/tty/!s/mesg n/tty -s \\&\\& mesg n/' /root/.profile
 #sudo /bin/bash --login -c "sudo cat /dev/null > /etc/issue && sudo cat /dev/null > /etc/issue.net"
+# TODO: create an ARGV that allows for condition to decide if config overwriting should be ignored (i.e. Vagrant)
+# or
+# overwritten (i.e. os updates w/in Jenkins builds) 
 sudo apt-get update
-sudo apt-get dist-upgrade -y
-sudo apt-get full-upgrade -y
+sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade -y
+sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" full-upgrade -y
 sudo apt autoremove -y
-sudo apt-get install -y apt-file
+sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install -y apt-file
 sudo apt-file update
