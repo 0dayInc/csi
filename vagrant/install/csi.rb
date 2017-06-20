32a98a343d4d3155f20f78aaf47a21efd61e6d25
Jacob Hoopes <jake.hoopes@gmail.com>
2017-06-20 11:56:47 -0600

    Vagrant - add additional installation dependencies

--- vagrant/install/csi.rb
+++ vagrant/install/csi.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 printf 'Installing CSI Dependencies ***********************************************************'
-`sudo apt-get install -y fontconfig postgresql-server-dev-all`
+`sudo apt-get install -y fontconfig postgresql-server-dev-all libpcap-dev libsndfile1 libsndfile1-dev libmagickwand-dev imagemagick tesseract-ocr-all`
 
 printf 'Performing Bundle Install and Installing csi Gem **************************************'
 `sudo bash --login -c "source /etc/profile.d/rvm.sh && cd /csi && gem install bundler && bundle install && rake && rake install && rake rerdoc && gem rdoc --rdoc --ri --overwrite -V csi"`
