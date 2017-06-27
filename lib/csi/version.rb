05d5acdde0ba65ae1bfcf2b2e25b393a086b7b87
Jacob Hoopes <jake.hoopes@gmail.com>
2017-06-27 15:31:44 -0600

    README.md - update to reflect new capabilities

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.2.153'
+  VERSION = '0.2.154'
 end
--- packer/provisioners/arachni.sh
+++ packer/provisioners/arachni.sh
@@ -4,4 +4,4 @@ arachni_build_path="${arachni_root}/arachni-1.5.1-0.5.12"
 arachni_pkg='arachni-1.5.1-0.5.12-linux-x86_64.tar.gz'
 arachni_url="https://github.com/Arachni/arachni/releases/download/v1.5.1/${arachni_pkg}"
 
-sudo /bin/bash --login -c "mkdir ${arachni_root} && cd ${arachni_root} && wget ${arachni_url} && tar -xzvf arachni-1.5.1-0.5.12-linux-x86_64.tar.gz && rm ${arachni_pkg}"
+sudo /bin/bash --login -c "mkdir ${arachni_root} && cd ${arachni_root} && wget ${arachni_url} && tar -xzvf arachni-1.5.1-0.5.12-linux-x86_64.tar.gz && rm ${arachni_pkg} && ls ${arachni_build_path}/bin | while read arachni_pkg; do ln -s ${arachni_build_path}/bin/${arachni_pkg} /usr/local/bin/; done"
