0d46875b2df093f6eea9b35875e7abdfb53eda3b
Jacob Hoopes <jake.hoopes@gmail.com>
2017-06-27 15:18:15 -0600

    Packer & Vagrant migrating install/update scripts to Packer provisioners

--- packer/provisioners/openvas.sh
+++ packer/provisioners/openvas.sh
@@ -1,2 +1,2 @@
 #!/bin/bash --login
-sudo /bin/bash --login -c 'apt-get install -y openvas && openvas-setup'
+sudo /bin/bash --login -c 'apt-get install -y rpm alien nsis openvas && openvas-setup && openvas-check-setup'
