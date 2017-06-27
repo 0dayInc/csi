92f5fbc4ab10745b09d8c5b2eb5ea45f1f9c3e35
Jacob Hoopes <jake.hoopes@gmail.com>
2017-06-26 20:24:00 -0600

    Packer - CSI installed w/ Virtualbox Provider

--- packer/provisioners/csi.sh
+++ packer/provisioners/csi.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get install -y fontconfig postgresql-server-dev-all libpcap-dev libsndfile1 libsndfile1-dev libmagickwand-dev imagemagick tesseract-ocr-all
 
-sudo bash --login -c "cd /csi && ./install.sh ruby-gem"
+sudo bash --login -c "cd /csi && cp etc/metasploit/msfrpcd.yaml.EXAMPLE etc/metasploit/msfrpcd.yaml && ./install.sh ruby-gem"
