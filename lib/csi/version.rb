8b18841063c5cc44f7e4b245650bf12b4a2ff163
Jacob Hoopes <jake.hoopes@gmail.com>
2017-06-27 15:42:47 -0600

    Packer - (bugfix) link all arachni binaries to /usr/local/bin

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.2.159'
+  VERSION = '0.2.160'
 end
--- packer/provisioners/arachni.sh
+++ packer/provisioners/arachni.sh
@@ -6,4 +6,4 @@ arachni_url="https://github.com/Arachni/arachni/releases/download/v1.5.1/${arach
 
 sudo /bin/bash --login -c "mkdir ${arachni_root} && cd ${arachni_root} && wget ${arachni_url} && tar -xzvf arachni-1.5.1-0.5.12-linux-x86_64.tar.gz && rm ${arachni_pkg}"
 
-sudo /bin/bash --login -c "ls ${arachni_build_path}/bin/ | while read arachni_bin; do ln -sf ${arachni_build_path}/bin/${arachni_bin} /usr/local/bin/; done"
+sudo /bin/bash --login -c "ls ${arachni_build_path}/bin/* | while read arachni_bin; do ln -sf ${arachni_build_path}/bin/${arachni_bin} /usr/local/bin/; done"
