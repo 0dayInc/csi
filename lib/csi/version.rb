09daa28bb81edb6e234d064fb72780a38a6dea87
Jacob Hoopes <jake.hoopes@gmail.com>
2017-06-27 09:10:40 -0600

    Packer - add VirtualBox Guest Additions Provisioner

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.2.145'
+  VERSION = '0.2.146'
 end
---
+++ packer/provisioners/virtualbox_guest_additions.sh
@@ -0,0 +1,3 @@
+#!/bin/bash --login
+printf "Installing VirtualBox Guest Additions **************************************************"
+sudo /bin/bash --login -c "apt-get install -y virtualbox-guest-x11"
