019a0ffa32cfcab9095fca386f07d6952413474d
Jacob Hoopes <jake.hoopes@gmail.com>
2017-06-27 15:22:00 -0600

    Packer - start openvas daemons

--- packer/provisioners/openvas.sh
+++ packer/provisioners/openvas.sh
@@ -1,2 +1,2 @@
 #!/bin/bash --login
-sudo /bin/bash --login -c 'apt-get install -y rpm alien nsis openvas && openvas-setup && openvas-check-setup'
+sudo /bin/bash --login -c 'apt-get install -y rpm alien nsis openvas && openvas-setup && openvas-check-setup && /etc/init.d/openvas-manager start && /etc/init.d/greenbone-security-assistant start'
