9ab61010cf21b2b2285bc291eafce41d43548755
Jacob Hoopes <jake.hoopes@gmail.com>
2017-06-28 09:24:58 -0600

    Packer & Vagrant - bugfix in csi provisioner for Vagrant & bugfix in update_os.sh provisioner for Packer/Vagrant

--- packer/provisioners/update_os.sh
+++ packer/provisioners/update_os.sh
@@ -7,4 +7,4 @@ sudo apt-get install -y debconf-utils
 
 # Automate Package Install Questions :)
 # Obtain values via: debconf-get-selections | less
-sudo /bin/bash --login -c "echo 'wireshark-common wireshark-common/install-setuid boolean false' | debconf-set-selections && apt-get -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' dist-upgrade -y && sudo apt-get -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' full-upgrade -y && apt autoremove -y && apt-get -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' install -y apt-file && apt-file update"
+sudo /bin/bash --login -c "echo 'grub-pc grub-pc/install_devices multiselect /dev/sda1' | debconf-set-selections && echo 'wireshark-common wireshark-common/install-setuid boolean false' | debconf-set-selections && apt-get -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' dist-upgrade -y && sudo apt-get -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' full-upgrade -y && apt autoremove -y && apt-get -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' install -y apt-file && apt-file update"
