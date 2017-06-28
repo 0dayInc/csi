f4d43d8c91228f00cb7a8984764c46a48c9b0a5e
Jacob Hoopes <jake.hoopes@gmail.com>
2017-06-28 09:13:35 -0600

    Packer - (bugfix) link all arachni binaries to /usr/local/bin & bugfix in openvas provisioner related to redis-server

--- packer/provisioners/arachni.sh
+++ packer/provisioners/arachni.sh
@@ -6,4 +6,6 @@ arachni_url="https://github.com/Arachni/arachni/releases/download/v1.5.1/${arach
 
 sudo /bin/bash --login -c "mkdir ${arachni_root} && cd ${arachni_root} && wget ${arachni_url} && tar -xzvf arachni-1.5.1-0.5.12-linux-x86_64.tar.gz && rm ${arachni_pkg}"
 
-sudo ls $arachni_build_path/bin/* | while read arachni_bin; do ln -sf $arachni_build_path/bin/$arachni_bin /usr/local/bin/; done
+ls $arachni_build_path/bin/* | while read arachni_bin; do 
+  sudo ln -sf $arachni_bin /usr/local/bin/
+done
--- packer/provisioners/openvas.sh
+++ packer/provisioners/openvas.sh
@@ -1,2 +1,2 @@
 #!/bin/bash --login
-sudo /bin/bash --login -c 'apt-get install -y rpm alien nsis openvas && openvas-setup && openvas-check-setup && /etc/init.d/openvas-manager start && /etc/init.d/greenbone-security-assistant start'
+sudo /bin/bash --login -c 'apt-get install -y rpm alien nsis openvas redis-server && systemctl enable redis-server && systemctl start redis-server && openvas-setup && openvas-check-setup && /etc/init.d/openvas-manager start && /etc/init.d/greenbone-security-assistant start'
