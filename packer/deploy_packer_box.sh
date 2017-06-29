f32cc0bcdfc1fcd38b717efea449698f5212902a
Jacob Hoopes <jake.hoopes@gmail.com>
2017-06-29 11:31:35 -0600

    Packer - enhance deploy_packer_box.sh to support multiple providers

--- packer/deploy_packer_box.sh
+++ packer/deploy_packer_box.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 packer_file=$1
+provider_type=$2
 set -e
 
 function usage() {
-  echo "USAGE: ${0} <packer_provider_template.json>"
+  echo "USAGE: ${0} <packer_provider_template.json> <provider type>"
   exit 1
 }
 
@@ -11,9 +12,24 @@ if [[ $# < 1 ]]; then
   usage
 fi
 
-#export PACKER_LOG=1
-rm kali_rolling_virtualbox.box || true
-#packer build -debug -only virtualbox-iso $packer_file
-packer build -only virtualbox-iso $packer_file
-vagrant box remove kali_rolling_virtualbox || true
-vagrant box add kali_rolling_virtualbox kali_rolling_virtualbox.box
+case $provider_type in
+  "virtualbox")
+    #export PACKER_LOG=1
+    rm kali_rolling_virtualbox.box || true
+    #packer build -debug -only virtualbox-iso $packer_file
+    packer build -only virtualbox-iso $packer_file
+    vagrant box remove kali_rolling_virtualbox || true
+    vagrant box add kali_rolling_virtualbox kali_rolling_virtualbox.box
+    ;;
+  "vmware")
+    #export PACKER_LOG=1
+    rm kali_rolling_vmware.box || true
+    #packer build -debug -only virtualbox-iso $packer_file
+    packer build -only vmware-iso $packer_file
+    vagrant box remove kali_rolling_vmware || true
+    vagrant box add kali_rolling_vmware kali_rolling_vmware.box
+    ;;
+  *)
+    usage
+    exit 1
+esac
