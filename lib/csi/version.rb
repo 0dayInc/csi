cde1ad55f3ae9af006d65feed9528fac6a20aebc
Jacob Hoopes <jake.hoopes@gmail.com>
2017-06-30 15:39:57 -0600

    Packer - provisioners/apache2.sh comment out daemon (for now)

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.2.209'
+  VERSION = '0.2.210'
 end
--- packer/provisioners/apache2.sh
+++ packer/provisioners/apache2.sh
@@ -8,4 +8,4 @@ sudo a2enmod ssl
 sudo a2enmod headers
 # Disable Version Headers
 sudo /bin/bash --login -c 'echo -e "ServerSignature Off\nServerTokens Prod" >> /etc/apache2/apache2.conf'
-sudo /etc/init.d/apache2 restart
+#sudo /etc/init.d/apache2 restart
