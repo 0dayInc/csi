089ad374bc8abb9eab726fa44f8134586c4dd69a
Jacob Hoopes <jake.hoopes@gmail.com>
2017-06-30 13:58:15 -0600

    Packer - provisioners/apache2.sh (bugfix in daemon restart)

--- packer/provisioners/apache2.sh
+++ packer/provisioners/apache2.sh
@@ -8,4 +8,4 @@ sudo a2enmod ssl
 sudo a2enmod headers
 # Disable Version Headers
 sudo /bin/bash --login -c 'echo -e "ServerSignature Off\nServerTokens Prod" >> /etc/apache2/apache2.conf'
-sudo systemctl start apache2
+sudo /etc/init.d/apache2 restart
