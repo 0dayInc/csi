32bac523486e6b2c5efdc22961c3a61eacfaa657
Jacob Hoopes <jake.hoopes@gmail.com>
2017-07-05 09:51:05 -0600

    Packer - Add docker_csi type

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.2.211'
+  VERSION = '0.2.216'
 end
--- packer/deploy_packer_box.sh
+++ packer/deploy_packer_box.sh
@@ -4,7 +4,7 @@ provider_type=$2
 set -e
 
 function usage() {
-  echo "USAGE: ${0} <packer_provider_template.json> <provider type>"
+  echo "USAGE: ${0} <packer_provider_template.json> <docker_csi||virtualbox||vmware>"
   exit 1
 }
 
@@ -13,6 +13,14 @@ if [[ $# < 1 ]]; then
 fi
 
 case $provider_type in
+  "docker_csi")
+    #export PACKER_LOG=1
+    rm kali_rolling_docker_csi.box || true
+    #packer build -debug -only docker $packer_file
+    packer build -only docker $packer_file
+    vagrant box remove kali_rolling_docker_csi || true
+    vagrant box add kali_rolling_docker_csi kali_rolling_docker_csi.box
+    ;;
   "virtualbox")
     #export PACKER_LOG=1
     rm kali_rolling_virtualbox.box || true
