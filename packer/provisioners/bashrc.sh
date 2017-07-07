43f66f628a3fbbdd27ddd861686e6a1aa73281d2
Jacob Hoopes <jake.hoopes@gmail.com>
2017-07-07 14:32:30 -0600

    Packer - added sip provisioner for WDaaS & bugfix in bashrc.sh provisioner

--- packer/provisioners/bashrc.sh
+++ packer/provisioners/bashrc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Initializes RVM for Normal Users
-sudo echo 'source /etc/profile.d/rvm.sh' >> /etc/bash.bashrc
+sudo /bin/bash --login -c 'echo "source /etc/profile.d/rvm.sh" >> /etc/bash.bashrc'
 
 # Used for proper phantomjs operation
-sudo echo 'export QT_QPA_PLATFORM=minimal' >> /etc/bash.bashrc
+sudo /bin/bash --login -c 'echo "export QT_QPA_PLATFORM=minimal" >> /etc/bash.bashrc'
