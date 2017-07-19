ed7157ce74e08631551bca894b924511a2a332d6
Jacob Hoopes <jake.hoopes@gmail.com>
2017-07-19 14:57:52 -0600

    Packer - rvm provisioner, trust gpg2 key

--- vagrant/provisioners/rvm.sh
+++ vagrant/provisioners/rvm.sh
@@ -2,8 +2,6 @@
 ruby_version=`cat /csi/.ruby-version`
 ruby_gemset=`cat /csi/.ruby-gemset`
 printf "Updating RVM..."
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-echo -e "trust\n5\ny\n" | gpg2 --command-fd 0 --edit-key 409B6B1796C275462A1703113804BB82D39DC0E3
 rvm get latest
 rvm reload
 /bin/bash --login -c "source /etc/profile.d/rvm.sh && rvm --default ${ruby_version}@${ruby_gemset}"
