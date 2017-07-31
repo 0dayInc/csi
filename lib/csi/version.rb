b6f1783ce1b96063bdb66c24265c036a3dbe4711
Jacob Hoopes <jake.hoopes@gmail.com>
2017-07-31 11:58:19 -0600

    Packer - add OpenVPN provisioner

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.2.286'
+  VERSION = '0.2.287'
 end
--- packer/provisioners/openvpn.sh
+++ packer/provisioners/openvpn.sh
@@ -1,2 +1,3 @@
 #!/bin/bash --login
-sudo apt-get install -y openvpn resolvconf
+# sudo apt-get install -y openvpn resolvconf
+sudo apt-get install -y openvpn
