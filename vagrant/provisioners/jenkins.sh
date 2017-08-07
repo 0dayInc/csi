8c158b6d5b5458452c552e4d1d6a23d68aa47939
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-07 15:30:50 -0600

    Vagrant - implement Userland jobs for Jenkins prior to deployment...i.e. add your own Jenkins jobs :)

--- vagrant/provisioners/jenkins.sh
+++ vagrant/provisioners/jenkins.sh
@@ -16,13 +16,72 @@ csi_jenkins_useradd -s 127.0.0.1 -S 8080 -u $new_user -p $new_pass -U admin -P $
 
 # Begin Creating Self-Update Jobs in Jenkins and Template-Based Jobs to Describe how to Intgrate CSI into Jenkins
 printf "Creating Self-Update Jobs *************************************************************"
-csi_jenkins_create_job --jenkins_ip 127.0.0.1 -U admin -P $initial_admin_pwd -j selfupdate-os -c /csi/etc/jenkins/jobs/selfupdate-os.xml
-csi_jenkins_create_job --jenkins_ip 127.0.0.1 -U admin -P $initial_admin_pwd -j selfupdate-rvm -c /csi/etc/jenkins/jobs/selfupdate-rvm.xml
-csi_jenkins_create_job --jenkins_ip 127.0.0.1 -U admin -P $initial_admin_pwd -j selfupdate-gem -c /csi/etc/jenkins/jobs/selfupdate-gem.xml
-csi_jenkins_create_job --jenkins_ip 127.0.0.1 -U admin -P $initial_admin_pwd -j selfupdate-wpscan -c /csi/etc/jenkins/jobs/selfupdate-wpscan.xml
-csi_jenkins_create_job --jenkins_ip 127.0.0.1 -U admin -P $initial_admin_pwd -j selfupdate-metasploit -c /csi/etc/jenkins/jobs/selfupdate-metasploit.xml
-csi_jenkins_create_job --jenkins_ip 127.0.0.1 -U admin -P $initial_admin_pwd -j selfupdate-ssllabs-scan -c /csi/etc/jenkins/jobs/selfupdate-ssllabs-scan.xml
-csi_jenkins_create_job --jenkins_ip 127.0.0.1 -U admin -P $initial_admin_pwd -j selfupdate-openvas_sync -c /csi/etc/jenkins/jobs/selfupdate-openvas_sync.xml
-csi_jenkins_create_job --jenkins_ip 127.0.0.1 -U admin -P $initial_admin_pwd -j selfupdate-csi -c /csi/etc/jenkins/jobs/selfupdate-csi.xml
-csi_jenkins_create_job --jenkins_ip 127.0.0.1 -U admin -P $initial_admin_pwd -j selfupdate-exploit-db -c /csi/etc/jenkins/jobs/selfupdate-exploit-db.xml
-csi_jenkins_create_job --jenkins_ip 127.0.0.1 -U admin -P $initial_admin_pwd -j anonymization-toggle_tor -c /csi/etc/jenkins/jobs/anonymization-toggle_tor.xml
+csi_jenkins_create_job --jenkins_ip 127.0.0.1 \
+  -U admin \
+  -P $initial_admin_pwd \
+  -j selfupdate-os \
+  -c /csi/etc/jenkins/jobs/selfupdate-os.xml
+
+csi_jenkins_create_job --jenkins_ip 127.0.0.1 \
+  -U admin \
+  -P $initial_admin_pwd \
+  -j selfupdate-rvm \
+  -c /csi/etc/jenkins/jobs/selfupdate-rvm.xml
+
+csi_jenkins_create_job --jenkins_ip 127.0.0.1 \
+  -U admin \
+  -P $initial_admin_pwd \
+  -j selfupdate-gem \
+  -c /csi/etc/jenkins/jobs/selfupdate-gem.xml
+
+csi_jenkins_create_job --jenkins_ip 127.0.0.1 \
+  -U admin \
+  -P $initial_admin_pwd \
+  -j selfupdate-wpscan \
+  -c /csi/etc/jenkins/jobs/selfupdate-wpscan.xml
+
+csi_jenkins_create_job --jenkins_ip 127.0.0.1 \
+  -U admin \
+  -P $initial_admin_pwd \
+  -j selfupdate-metasploit \
+  -c /csi/etc/jenkins/jobs/selfupdate-metasploit.xml
+
+csi_jenkins_create_job --jenkins_ip 127.0.0.1 \
+  -U admin \
+  -P $initial_admin_pwd \
+  -j selfupdate-ssllabs-scan \
+  -c /csi/etc/jenkins/jobs/selfupdate-ssllabs-scan.xml
+
+csi_jenkins_create_job --jenkins_ip 127.0.0.1 \
+  -U admin \
+  -P $initial_admin_pwd \
+  -j selfupdate-openvas_sync \
+  -c /csi/etc/jenkins/jobs/selfupdate-openvas_sync.xml
+
+csi_jenkins_create_job --jenkins_ip 127.0.0.1 \
+  -U admin \
+  -P $initial_admin_pwd \
+  -j selfupdate-csi \
+  -c /csi/etc/jenkins/jobs/selfupdate-csi.xml
+
+csi_jenkins_create_job --jenkins_ip 127.0.0.1 \
+  -U admin \
+  -P $initial_admin_pwd \
+  -j selfupdate-exploit-db \
+  -c /csi/etc/jenkins/jobs/selfupdate-exploit-db.xml
+
+csi_jenkins_create_job --jenkins_ip 127.0.0.1 \
+  -U admin 
+  -P $initial_admin_pwd \
+  -j anonymization-toggle_tor \
+  -c /csi/etc/jenkins/jobs/anonymization-toggle_tor.xml
+
+# Create any jobs residing in /csi/jenkins/jobs_userland
+ls /csi/etc/jenkins/jobs_userland/ | while read jenkins_xml_config; do
+  job_name=${jenkins_xml_config%.*}
+  csi_jenkins_create_job --jenkins_ip 127.0.0.1 \
+    -U admin \
+    -P $initial_admin_pwd \
+    -j $job_name \
+    -c /csi/etc/jenkins/jobs_userland/$jenkins_xml_config
+done
