2bea8ece7508f15ef24103baaac6116690b214c5
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-07 11:58:46 -0600

    Packer - add post-processor to upload csi/kali_rolling boxes to respective provider in Vagrant Cloud

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.2.290'
+  VERSION = '0.2.291'
 end
--- packer/deploy_packer_box.sh
+++ packer/deploy_packer_box.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-packer_file=$1
-provider_type=$2
+provider_type=$1
 set -e
 
 function usage() {
-  echo "USAGE: ${0} <packer_provider_template.json> <docker||docker_csi||virtualbox||vmware>"
+  echo "USAGE: ${0} <docker||docker_csi||virtualbox||vmware>"
   exit 1
 }
 
-if [[ $# < 2 ]]; then
+if [[ $# < 1 ]]; then
   usage
 fi
 
@@ -16,32 +15,28 @@ case $provider_type in
   "docker")
     #export PACKER_LOG=1
     rm kali_rolling_docker.box || true
-    #packer build -debug -only docker $packer_file
-    packer build -only docker $packer_file
+    packer build -only docker kali_rolling_docker.json
     vagrant box remove csi/kali_rolling --provider=docker|| true
     vagrant box add csi/kali_rolling kali_rolling_docker.box
     ;;
   "docker_csi")
     #export PACKER_LOG=1
     rm kali_rolling_docker_csi.box || true
-    #packer build -debug -only docker $packer_file
-    packer build -only docker $packer_file
+    packer build -only docker kali_rolling_docker_csi.json
     vagrant box remove csi/prototyper --provider=docker || true
     vagrant box add csi/prototyper kali_rolling_docker_csi.box
     ;;
   "virtualbox")
     #export PACKER_LOG=1
     rm kali_rolling_virtualbox.box || true
-    #packer build -debug -only virtualbox-iso $packer_file
-    packer build -only virtualbox-iso $packer_file
+    packer build -only virtualbox-iso -var-file=packer_secrets.json kali_rolling_virtualbox.json
     vagrant box remove csi/kali_rolling --provider=virtualbox || true
     vagrant box add csi/kali_rolling kali_rolling_virtualbox.box
     ;;
   "vmware")
     #export PACKER_LOG=1
     rm kali_rolling_vmware.box || true
-    #packer build -debug -only virtualbox-iso $packer_file
-    packer build -only vmware-iso $packer_file
+    packer build -only vmware-iso -var-file=packer_secrets.json kali_rolling_vmware.json
     vagrant box remove csi/kali_rolling --provider=vmware_desktop || true
     vagrant box add csi/kali_rolling kali_rolling_vmware.box
     ;;
