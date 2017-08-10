036159b827cdef7dca3249dbb9759c043b19ccd1
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-10 11:06:09 -0600

    Packer - add rc.local.sh provisioner to support running simple tasks @ boot time

--- packer/provisioners/burpsuite.sh
+++ packer/provisioners/burpsuite.sh
@@ -7,10 +7,4 @@ burp_root="/opt/burpsuite"
 burpbuddy_build_root="${burp_root}/burpbuddy/burp"
 sudo /bin/bash --login -c "mkdir ${burp_root} && cp -a ${burpbuddy_tp_root} ${burp_root} && cd ${burpbuddy_build_root} && mvn package && cp ${burpbuddy_build_root}/target/burpbuddy-2.3.1.jar ${burp_root}"
 
-sudo /bin/bash --login -c 'echo "#!/bin/sh -e" > /etc/rc.local'
-sudo /bin/bash --login -c 'echo "ifconfig lo:0 127.0.0.2 netmask 255.0.0.0 up" >> /etc/rc.local'
-sudo /bin/bash --login -c 'echo "ifconfig lo:1 127.0.0.3 netmask 255.0.0.0 up" >> /etc/rc.local'
-sudo /bin/bash --login -c 'echo "ifconfig lo:2 127.0.0.4 netmask 255.0.0.0 up" >> /etc/rc.local'
-sudo /bin/bash --login -c 'chmod 755 /etc/rc.local'
-
 #TODO: Install Burpsuite Pro? (May need to rely upon end-user to do this)
