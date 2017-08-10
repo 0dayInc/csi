a133855f5db9459145ca8aefc6498c5fe698a473
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-10 11:24:40 -0600

    Packer - add rc.local.sh provisioner to support running simple tasks @ boot time

--- packer/provisioners/rc.local.sh
+++ packer/provisioners/rc.local.sh
@@ -2,9 +2,11 @@
 # Configure simple tasks to run @ boot
 
 sudo /bin/bash --login -c 'echo "#!/bin/sh -e" > /etc/rc.local'
+
 sudo /bin/bash --login -c 'echo "ifconfig lo:0 127.0.0.2 netmask 255.0.0.0 up" >> /etc/rc.local'
 sudo /bin/bash --login -c 'echo "ifconfig lo:1 127.0.0.3 netmask 255.0.0.0 up" >> /etc/rc.local'
 sudo /bin/bash --login -c 'echo "ifconfig lo:2 127.0.0.4 netmask 255.0.0.0 up" >> /etc/rc.local'
 sudo /bin/bash --login -c 'echo "tor &" >> /etc/rc.local'
 
+sudo /bin/bash --login -c 'echo "exit 0" >> /etc/rc.local'
 sudo /bin/bash --login -c 'chmod 755 /etc/rc.local'
