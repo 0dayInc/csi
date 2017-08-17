ecc2c08b86948197c9572a8e74f6cb62c29600fe
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-17 10:55:02 -0600

    Packer - minor bugfix in virtualbox_guest_additions provisioner

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.2.314'
+  VERSION = '0.2.315'
 end
--- packer/provisioners/virtualbox_guest_additions.sh
+++ packer/provisioners/virtualbox_guest_additions.sh
@@ -1,2 +1,3 @@
 #!/bin/bash --login
+sudo apt-get purge -y virtualbox-*
 sudo apt-get install -y virtualbox-guest-x11
