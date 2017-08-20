a2ccfc97224807e87868f4be21e16d7caae00330
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-20 09:46:13 -0600

    Vagrant - bugfix in update_jenkins_plugins.rb & selfupdate-jenkins_plugins.xml Jenkins job #cleanup

--- vagrant/provisioners/update_jenkins_plugins.rb
+++ vagrant/provisioners/update_jenkins_plugins.rb
@@ -3,13 +3,9 @@
 
 require 'yaml'
 
-print 'Updating Jenkins Plugins...'
-# ruby_version = File.readlines('/csi/.ruby-version')[0].to_s.scrub.strip.chomp
-# rvm_gemset = File.readlines('/csi/.ruby-gemset')[0].to_s.scrub.strip.chomp
-
 jenkins_userland_config = YAML.load_file('/csi/etc/jenkins/vagrant.yaml')
 userland_user = jenkins_userland_config['user']
 userland_pass = jenkins_userland_config['pass']
 
-# puts `/bin/bash --login -c "source /etc/profile.d/rvm.sh; rvm use ruby-#{ruby_version}@#{rvm_gemset}; csi_jenkins_update_plugins -s '127.0.0.1' -d 8888 -U #{userland_user} -p #{userland_pass}"`
+print 'Updating Jenkins Plugins...'
 puts `/bin/bash --login -c "csi_jenkins_update_plugins -s '127.0.0.1' -d 8888 -U #{userland_user} -p #{userland_pass}"`
