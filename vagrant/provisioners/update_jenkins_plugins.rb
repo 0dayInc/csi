261d37fbd77006a935d8cec68c1b630c6e35cf87
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-20 09:33:36 -0600

    Vagrant - bugfix in update_jenkins_plugins.rb & selfupdate-jenkins_plugins.xml Jenkins job

--- vagrant/provisioners/update_jenkins_plugins.rb
+++ vagrant/provisioners/update_jenkins_plugins.rb
@@ -4,11 +4,12 @@
 require 'yaml'
 
 print 'Updating Jenkins Plugins...'
-ruby_version = File.readlines('/csi/.ruby-version')[0].to_s.scrub.strip.chomp
-rvm_gemset = File.readlines('/csi/.ruby-gemset')[0].to_s.scrub.strip.chomp
+# ruby_version = File.readlines('/csi/.ruby-version')[0].to_s.scrub.strip.chomp
+# rvm_gemset = File.readlines('/csi/.ruby-gemset')[0].to_s.scrub.strip.chomp
 
 jenkins_userland_config = YAML.load_file('/csi/etc/jenkins/vagrant.yaml')
 userland_user = jenkins_userland_config['user']
 userland_pass = jenkins_userland_config['pass']
 
-puts `/bin/bash --login -c "source /etc/profile.d/rvm.sh; rvm use ruby-#{ruby_version}@#{rvm_gemset}; csi_jenkins_update_plugins -s '127.0.0.1' -d 8888 -U #{userland_user} -p #{userland_pass}"`
+# puts `/bin/bash --login -c "source /etc/profile.d/rvm.sh; rvm use ruby-#{ruby_version}@#{rvm_gemset}; csi_jenkins_update_plugins -s '127.0.0.1' -d 8888 -U #{userland_user} -p #{userland_pass}"`
+puts `/bin/bash --login -c "csi_jenkins_update_plugins -s '127.0.0.1' -d 8888 -U #{userland_user} -p #{userland_pass}"`
