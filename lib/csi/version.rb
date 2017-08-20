d2f0a1f7165602ff985093d37537412cb686fa85
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-20 09:11:46 -0600

    Vagrant - bugfix in update_jenkins_plugins.rb & selfupdate-jenkins_plugins.xml Jenkins job

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.2.319'
+  VERSION = '0.2.320'
 end
--- vagrant/provisioners/update_jenkins_plugins.rb
+++ vagrant/provisioners/update_jenkins_plugins.rb
@@ -11,4 +11,4 @@ jenkins_userland_config = YAML.load_file('/csi/etc/jenkins/vagrant.yaml')
 userland_user = jenkins_userland_config['user']
 userland_pass = jenkins_userland_config['pass']
 
-puts `sudo bash --login -c "source /etc/profile.d/rvm.sh; rvm use ruby-#{ruby_version}@#{rvm_gemset}; csi_jenkins_update_plugins -s '127.0.0.1' -d 8888 -U #{userland_user} -p #{userland_pass}"`
+puts `/bin/bash --login -c "source /etc/profile.d/rvm.sh; rvm use ruby-#{ruby_version}@#{rvm_gemset}; csi_jenkins_update_plugins -s '127.0.0.1' -d 8888 -U #{userland_user} -p #{userland_pass}"`
