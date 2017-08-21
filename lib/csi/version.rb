70d3fac8b6c15030ee507aef57af87f952cb30c0
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-21 13:07:39 -0600

    Packer - change afl provisioner to build from source in order to fuzz binaries where code is unavailable (i.e. qemu instrumentation)

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.2.335'
+  VERSION = '0.2.336'
 end
--- packer/provisioners/afl.sh
+++ packer/provisioners/afl.sh
@@ -1,2 +1,7 @@
 #!/bin/bash --login
-sudo apt-get install -y afl afl-clang afl-cov afl-doc
+# Build from source in order to support Qemu Instrumentation:
+# Found in https://github.com/mirrorer/afl/README.md => "4) Instrumenting binary-only apps"
+# sudo apt-get purge -y afl afl-clang afl-cov afl-doc
+# sudo apt autoremove -y
+sudo apt install libtool libtool-bin libglib2.0-dev
+sudo /bin/bash --login -c 'cd /opt && git clone https://github.com/mirrorer/afl && cd /opt/afl && make && cd /opt/afl/qemu_mode && ./build_qemu_support.sh'
