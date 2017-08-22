141c2717bd93d1c73577264c756afa1fef38b27a
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-22 10:09:57 -0600

    Packer - shift order of execution of OpenVAS sync executables & symlink afl binaries to /usr/local/bin

--- packer/provisioners/afl.sh
+++ packer/provisioners/afl.sh
@@ -5,3 +5,7 @@
 # sudo apt autoremove -y
 sudo apt install -y libtool libtool-bin libglib2.0-dev
 sudo /bin/bash --login -c 'cd /opt && git clone https://github.com/mirrorer/afl && cd /opt/afl && make && cd /opt/afl/qemu_mode && ./build_qemu_support.sh'
+
+ls -l | grep '^-rwx' | awk '{print $9}' | while read afl_bin; do 
+  sudo ln -s /opt/afl/$afl_bin /usr/local/bin/
+done
