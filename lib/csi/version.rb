5cdd25a738217566cc5dcbfefbcb0450e2228a7c
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-22 12:46:38 -0600

    Vagrant - begin putting together a BurpSuite Pro provisioner

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.2.349'
+  VERSION = '0.2.350'
 end
--- vagrant/provisioners/burpsuite_pro.rb
+++ vagrant/provisioners/burpsuite_pro.rb
@@ -4,11 +4,11 @@
 require 'yaml'
 
 userland_burpsuite_pro_jar_path = '/csi/third_party/burpsuite-pro.jar'
-burpsuite_pro_yaml = YAML.load_file('/csi/etc/burpsuite/vagrant.yaml')
-burpsuite_pro_jar_sha256_sum = burpsuite_pro_yaml['burpsuite_pro_jar_sha256_sum']
-license_key = burpsuite_pro_yaml['license_key'].to_s.scrub.strip.chomp
-
 if File.exist?(userland_burpsuite_pro_jar_path)
+  burpsuite_pro_yaml = YAML.load_file('/csi/etc/burpsuite/vagrant.yaml')
+  burpsuite_pro_jar_sha256_sum = burpsuite_pro_yaml['burpsuite_pro_jar_sha256_sum']
+  license_key = burpsuite_pro_yaml['license_key'].to_s.scrub.strip.chomp
+
   this_sha256_sum = Digest::SHA256.file(userland_burpsuite_pro_jar_path).to_s
 
   if this_sha256_sum == burpsuite_pro_jar_sha256_sum
