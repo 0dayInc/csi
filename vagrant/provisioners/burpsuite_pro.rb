40c3430d2e6009922f4165da0e895d7c9e5a3a71
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-22 15:12:44 -0600

    Vagrant - initial working version of BurpSuite Pro provisioner

--- vagrant/provisioners/burpsuite_pro.rb
+++ vagrant/provisioners/burpsuite_pro.rb
@@ -2,8 +2,11 @@
 # frozen_string_literal: true
 
 require 'yaml'
+require 'digest'
+require 'fileutils'
 
 userland_burpsuite_pro_jar_path = '/csi/third_party/burpsuite-pro.jar'
+burpsuite_pro_jar_dest_path = "/opt/burpsuite/#{File.basename(userland_burpsuite_pro_jar_path)}"
 if File.exist?(userland_burpsuite_pro_jar_path)
   burpsuite_pro_yaml = YAML.load_file('/csi/etc/burpsuite/vagrant.yaml')
   burpsuite_pro_jar_sha256_sum = burpsuite_pro_yaml['burpsuite_pro_jar_sha256_sum']
@@ -12,6 +15,8 @@ if File.exist?(userland_burpsuite_pro_jar_path)
   this_sha256_sum = Digest::SHA256.file(userland_burpsuite_pro_jar_path).to_s
 
   if this_sha256_sum == burpsuite_pro_jar_sha256_sum
-    # TODO: rsync userland_burpsuite_pro_jar_path to /opt/burpsuite
+    print "Discovered Userland BurpSuite Pro jar...copying to #{burpsuite_pro_jar_dest_path}"
+    FileUtils.cp(userland_burpsuite_pro_jar_path, burpsuite_pro_jar_dest_path)
+    puts 'complete.'
   end
 end
