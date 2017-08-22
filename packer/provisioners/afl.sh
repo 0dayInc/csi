a11b8f6d54a0e385c1778fb7789bcb8facf876f0
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-21 18:59:25 -0600

    Packer - bugfix in afl provisioner

--- packer/provisioners/afl.sh
+++ packer/provisioners/afl.sh
@@ -3,5 +3,5 @@
 # Found in https://github.com/mirrorer/afl/README.md => "4) Instrumenting binary-only apps"
 # sudo apt-get purge -y afl afl-clang afl-cov afl-doc
 # sudo apt autoremove -y
-sudo apt install libtool libtool-bin libglib2.0-dev
+sudo apt install -y libtool libtool-bin libglib2.0-dev
 sudo /bin/bash --login -c 'cd /opt && git clone https://github.com/mirrorer/afl && cd /opt/afl && make && cd /opt/afl/qemu_mode && ./build_qemu_support.sh'
