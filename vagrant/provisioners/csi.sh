16e4d96b73d5f05ae59f28548d5862c88867c3ec
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-23 16:30:21 -0600

    Vagrant - csi.sh provisioner only update csi w/ perms of vagrant user

--- vagrant/provisioners/csi.sh
+++ vagrant/provisioners/csi.sh
@@ -1,2 +1,2 @@
 #!/bin/bash --login
-sudo bash --login -c 'cd /csi && ./reinstall_csi_gemset.sh && ./build_csi_gem.sh'
+sudo -H -u vagrant /bin/bash --login -c 'cd /csi && ./reinstall_csi_gemset.sh && ./build_csi_gem.sh'
