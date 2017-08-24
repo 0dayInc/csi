c8fffb0a71d21d99172780f77ac7579183df3295
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-23 20:58:20 -0600

    Vagrant - csi.sh provisioner only update csi w/ perms of vagrant user #rollback

--- packer/provisioners/csi.sh
+++ packer/provisioners/csi.sh
@@ -57,4 +57,4 @@ case $os in
     exit 1
 esac
 
-sudo bash --login -c 'cd /csi && cp etc/metasploit/vagrant.yaml.EXAMPLE etc/metasploit/vagrant.yaml && sudo ./reinstall_csi_gemset.sh && ./build_csi_gem.sh && rubocop'
+sudo --login -c 'cd /csi && cp etc/metasploit/vagrant.yaml.EXAMPLE etc/metasploit/vagrant.yaml && sudo ./reinstall_csi_gemset.sh && ./build_csi_gem.sh && rubocop'
--- vagrant/provisioners/csi.sh
+++ vagrant/provisioners/csi.sh
@@ -1,2 +1,2 @@
 #!/bin/bash --login
-sudo -H -u vagrant /bin/bash --login -c 'cd /csi && ./reinstall_csi_gemset.sh && ./build_csi_gem.sh'
+sudo /bin/bash --login -c 'cd /csi && ./reinstall_csi_gemset.sh && ./build_csi_gem.sh'
