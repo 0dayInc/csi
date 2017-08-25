9982b991d9559ff453bb035fe777bd291a6cb020
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-25 11:55:59 -0600

    Packer - add contextual comment around usage for pwntools provisioner

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.2.372'
+  VERSION = '0.2.373'
 end
--- packer/provisioners/pwntools.sh
+++ packer/provisioners/pwntools.sh
@@ -1,3 +1,6 @@
 #!/bin/bash --login
 # Quick Prototyping of Exploits
+# $ ipython
+# in [1]: from pwn import *
+# More information available here: https://docs.pwntools.com/en/stable/
 sudo -H pip install pwntools
