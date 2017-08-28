9907fcd626d95b2a86c5a474eb18165477fa1681
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-28 14:56:53 -0600

    Vagrant - add jenkins_ssh-keygen provisioner to allow Userland jobs to communicate w/ other hosts via ssh

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.2.382'
+  VERSION = '0.2.383'
 end
---
+++ vagrant/provisioners/jenkins_ssh-keygen.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'yaml'
+
+jenkins_userland_config = YAML.load_file('/csi/etc/jenkins/vagrant.yaml')
+userland_ssh_keygen_pass = jenkins_userland_config['ssh_keygen_pass']
+hostname = `hostname -d`.to_s.chomp.strip.scrub
+
+print 'Creating SSH Key for Userland Jenkins Jobs...'
+puts `sudo -H -u jenkins ssh-keygen -t rsa -b 4096 -C jenkins@#{hostname} -N #{userland_ssh_keygen_pass} -f '/var/lib/jenkins/.ssh/id_rsa-csi_jenkins'`
