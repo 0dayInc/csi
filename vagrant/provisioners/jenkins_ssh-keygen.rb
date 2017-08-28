04d9d9af1c28ae3c4fedba03c6ecff2d729cbcfc
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-28 15:03:47 -0600

    Vagrant - add jenkins_ssh-keygen provisioner hostname to comments

--- vagrant/provisioners/jenkins_ssh-keygen.rb
+++ vagrant/provisioners/jenkins_ssh-keygen.rb
@@ -5,7 +5,7 @@ require 'yaml'
 
 jenkins_userland_config = YAML.load_file('/csi/etc/jenkins/vagrant.yaml')
 userland_ssh_keygen_pass = jenkins_userland_config['ssh_keygen_pass']
-hostname = `hostname -d`.to_s.chomp.strip.scrub
+hostname = `hostname`.to_s.chomp.strip.scrub
 
 print 'Creating SSH Key for Userland Jenkins Jobs...'
 puts `sudo -H -u jenkins ssh-keygen -t rsa -b 4096 -C jenkins@#{hostname} -N #{userland_ssh_keygen_pass} -f '/var/lib/jenkins/.ssh/id_rsa-csi_jenkins'`
