160c7bb6c6f5d36a0bfe13df940677fe3faf47bd
Jacob Hoopes <jake.hoopes@gmail.com>
2017-09-26 15:58:44 -0600

    CSI::Plugins::OwaspZap - prevent the kernel from killing the spawned pid

--- lib/csi/plugins/owasp_zap.rb
+++ lib/csi/plugins/owasp_zap.rb
@@ -128,7 +128,7 @@ module CSI
           stdout.each do |line|
             puts line
             if line.include?(return_pattern)
-              Process.daemon(nochdir=true,noclose=true)
+              Process.daemon(nochdir = true, noclose = true)
               # Process.detach(pid)
             end
           end
