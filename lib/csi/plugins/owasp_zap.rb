ddf543d33b7344322e1ac47e3e07bb7e8facb981
Jacob Hoopes <jake.hoopes@gmail.com>
2017-09-26 18:19:55 -0600

    CSI::Plugins::OwaspZap - prevent the kernel from killing the spawned pid

--- lib/csi/plugins/owasp_zap.rb
+++ lib/csi/plugins/owasp_zap.rb
@@ -120,26 +120,20 @@ module CSI
         zap_obj[:host] = proxy_uri.host.to_s.scrub
         zap_obj[:port] = proxy_uri.port.to_i
 
+        fork_stdout_arr = []
         fork_pid = Process.fork do
-          @fork_stdout = StringIO.new
           PTY.spawn(owasp_zap_cmd) do |stdout, _stdin, pid|
             zap_obj[:pid] = pid
             stdout.sync = true
             stdout.each do |line|
-              @fork_stdout.puts line
+              fork_stdout_arr.push(line)
             end
           end
         end
         Process.detach(fork_pid)
 
         return_pattern = '[AWT-EventQueue-1] INFO hsqldb.db..ENGINE  - Database closed'
-        # sleep 3 until fork_stdout.string.include?(return_pattern)
-        until @fork_stdout.string.include?(return_pattern)
-          puts @fork_stdout.string
-          puts "\n\n\n"
-          sleep 3
-        end
-
+        sleep 3 until fork_stdout_arr.include?(return_pattern)
         return zap_obj
       rescue => e
         stop(zap_obj) unless zap_obj.nil?
--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.2.491'
+  VERSION = '0.2.492'
 end
