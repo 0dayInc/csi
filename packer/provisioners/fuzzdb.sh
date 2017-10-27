b29f332000e92edd4f7ec8efb149e8c2f543e9e0
Jacob Hoopes <jake.hoopes@gmail.com>
2017-10-27 11:09:32 -0600

    Packer - rename fuzzdb-git to fuzzdb-dev in fuzzdb provisioner

--- packer/provisioners/fuzzdb.sh
+++ packer/provisioners/fuzzdb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo /bin/bash --login -c "cd /opt && git clone https://github.com/fuzzdb-project/fuzzdb.git fuzzdb-git"
+sudo /bin/bash --login -c "cd /opt && git clone https://github.com/fuzzdb-project/fuzzdb.git fuzzdb-dev"
