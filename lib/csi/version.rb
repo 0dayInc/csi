306757e9ecb91da6caf109b968316688910ac39f
Jacob Hoopes <jake.hoopes@gmail.com>
2017-11-22 16:58:05 -0700

    Packer - add debugging capabilities to deploy_packer_box.sh

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.2.705'
+  VERSION = '0.2.706'
 end
--- packer/deploy_packer_box.sh
+++ packer/deploy_packer_box.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 provider_type=$1
 box_version=$2
+debug=false
 set -e
 
 function usage() {
-  echo "USAGE: ${0} <docker||docker_csi||virtualbox||vmware> <box version to build e.g. 2017.3.001>"
+  echo "USAGE: ${0} <docker||docker_csi||virtualbox||vmware> <box version to build e.g. 2017.3.001> <debug>"
   exit 1
 }
 
@@ -12,40 +13,78 @@ if [[ $# < 2 ]]; then
   usage
 fi
 
+if [[ $3 != '' ]]; then
+  debug=true
+  export PACKER_LOG=1
+fi
+
 case $provider_type in
   "docker")
-    #export PACKER_LOG=1
     rm kali_rolling_docker.box || true
-    packer build -only docker kali_rolling_docker.json
+    if $debug; then
+      packer build \
+        -debug \
+        -only docker \
+        kali_rolling_docker.json
+    else
+      packer build \
+        -only docker \
+        kali_rolling_docker.json
+    fi
     vagrant box remove csi/kali_rolling --provider=docker|| true
     vagrant box add csi/kali_rolling kali_rolling_docker.box
     ;;
   "docker_csi")
-    #export PACKER_LOG=1
     rm kali_rolling_docker_csi.box || true
-    packer build -only docker kali_rolling_docker_csi.json
+    if $debug; then
+      packer build \
+        -debug \
+        -only docker \
+        kali_rolling_docker_csi.json
+    else
+      packer build \
+        -only docker \
+        kali_rolling_docker_csi.json
+    fi
     vagrant box remove csi/prototyper --provider=docker || true
     vagrant box add csi/prototyper kali_rolling_docker_csi.box
     ;;
   "virtualbox")
-    #export PACKER_LOG=1
     rm kali_rolling_virtualbox.box || true
-    packer build \
-      -only virtualbox-iso \
-      -var "box_version=${box_version}" \
-      -var-file=packer_secrets.json \
-      kali_rolling_virtualbox.json
+    if $debug; then
+      packer build \
+        -debug \
+        -only virtualbox-iso \
+        -var "box_version=${box_version}" \
+        -var-file=packer_secrets.json \
+        kali_rolling_virtualbox.json
+    else
+      packer build \
+        -only virtualbox-iso \
+        -var "box_version=${box_version}" \
+        -var-file=packer_secrets.json \
+        kali_rolling_virtualbox.json
+    fi
     vagrant box remove csi/kali_rolling --provider=virtualbox || true
     vagrant box add csi/kali_rolling kali_rolling_virtualbox.box
     ;;
   "vmware")
-    #export PACKER_LOG=1
+    echo $debug
     rm kali_rolling_vmware.box || true
-    packer build \
-      -only vmware-iso \
-      -var "box_version=${box_version}" \
-      -var-file=packer_secrets.json \
-      kali_rolling_vmware.json
+    if $debug; then
+      packer build \
+        -debug \
+        -only vmware-iso \
+        -var "box_version=${box_version}" \
+        -var-file=packer_secrets.json \
+        kali_rolling_vmware.json
+    else
+      packer build \
+        -only vmware-iso \
+        -var "box_version=${box_version}" \
+        -var-file=packer_secrets.json \
+        kali_rolling_vmware.json
+    fi
     vagrant box remove csi/kali_rolling --provider=vmware_desktop || true
     vagrant box add csi/kali_rolling kali_rolling_vmware.box
     ;;
