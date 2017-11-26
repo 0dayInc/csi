a87a1fe3cdd61a3e50ad640596ce4a8740592d94
Jacob Hoopes <jake.hoopes@gmail.com>
2017-11-26 15:50:20 -0700

    Packer - update pwntools provisioner to reflect new build procedures

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.2.714'
+  VERSION = '0.2.715'
 end
--- packer/provisioners/pwntools.sh
+++ packer/provisioners/pwntools.sh
@@ -3,4 +3,4 @@
 # $ ipython
 # in [1]: from pwn import *
 # More information available here: https://docs.pwntools.com/en/stable/
-sudo -H pip install pwntools
+sudo -H /bin/bash --login -c "apt-get update && apt-get install python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential && pip install --upgrade pip && pip install --upgrade pwntools"
