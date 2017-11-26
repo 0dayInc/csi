69c242a62532bf06e2ca91ce05ec7d85c66b170a
Jacob Hoopes <jake.hoopes@gmail.com>
2017-11-26 13:18:47 -0700

    Packer - dynam-i-size deploy_packer_box.sh #bugfixes

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.2.713'
+  VERSION = '0.2.714'
 end
--- packer/deploy_packer_box.sh
+++ packer/deploy_packer_box.sh
@@ -43,26 +43,26 @@ fi
 case $provider_type in
   "docker")
     rm kali_rolling_docker.box || true
-    pack(docker, kali_rolling_docker.json, $debug)
+    pack docker kali_rolling_docker.json $debug
     vagrant box remove csi/kali_rolling --provider=docker|| true
     vagrant box add csi/kali_rolling kali_rolling_docker.box
     ;;
   "docker_csi")
     rm kali_rolling_docker_csi.box || true
-    pack(docker, kali_rolling_docker_csi.json, $debug)
+    pack docker kali_rolling_docker_csi.json $debug
     vagrant box remove csi/prototyper --provider=docker || true
     vagrant box add csi/prototyper kali_rolling_docker_csi.box
     ;;
   "virtualbox")
     rm kali_rolling_virtualbox.box || true
-    pack(virtualbox-iso, kali_rolling_virtualbox.json, $debug)
+    pack virtualbox-iso kali_rolling_virtualbox.json $debug
     vagrant box remove csi/kali_rolling --provider=virtualbox || true
     vagrant box add csi/kali_rolling kali_rolling_virtualbox.box
     ;;
   "vmware")
     echo $debug
     rm kali_rolling_vmware.box || true
-    pack(vmware-iso, kali_rolling_vmware.json, $debug)
+    pack vmware-iso kali_rolling_vmware.json $debug
     vagrant box remove csi/kali_rolling --provider=vmware_desktop || true
     vagrant box add csi/kali_rolling kali_rolling_vmware.box
     ;;
