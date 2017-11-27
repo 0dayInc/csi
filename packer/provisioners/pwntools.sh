cd28aebd11642420eb09ae08b78c1aa117d8dbb8
Jacob Hoopes <jake.hoopes@gmail.com>
2017-11-26 17:08:16 -0700

    Packer - update pwntools provisioner to reflect new build procedures

--- packer/provisioners/pwntools.sh
+++ packer/provisioners/pwntools.sh
@@ -3,4 +3,4 @@
 # $ ipython
 # in [1]: from pwn import *
 # More information available here: https://docs.pwntools.com/en/stable/
-sudo -H /bin/bash --login -c "apt-get update && apt-get install python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential && pip install --upgrade pip && pip install --upgrade pwntools"
+sudo /bin/bash --login -c "apt update && apt install -y python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential && pip install --upgrade pip && pip install --upgrade pwntools"
