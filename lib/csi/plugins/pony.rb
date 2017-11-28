944454964c648e214351e7f53f7b7abe9f67c4af
Jacob Hoopes <jake.hoopes@gmail.com>
2017-11-28 12:46:58 -0700

    CSI::Plugins::Pony - bugfix in missing method

--- lib/csi/plugins/pony.rb
+++ lib/csi/plugins/pony.rb
@@ -197,7 +197,7 @@ module CSI
             body options[:body]
           end
 
-          delivery_method options[:via], options[:via_options]
+          default_delivery_method options[:via], options[:via_options]
         end
 
         (options[:headers] ||= {}).each do |key, value|
--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.2.726'
+  VERSION = '0.2.727'
 end
