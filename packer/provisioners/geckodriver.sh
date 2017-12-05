20d5a287921c4b79ed1a3d81d8ac6e2be4c95015
Jacob Hoopes <jake.hoopes@gmail.com>
2017-12-05 11:49:52 -0700

    Packer - update GeckoDriver plugin for automated Firefox browsing

--- packer/provisioners/geckodriver.sh
+++ packer/provisioners/geckodriver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash --login
-firefox_plugin_path='/csi/third_party/geckodriver-v0.13.0-linux64.tar.gz'
+firefox_plugin_path='/csi/third_party/geckodriver-v0.19.1-linux64.tar.gz'
 sudo tar -xzvf $firefox_plugin_path -C /usr/local/bin
