d165acd08b33473f9b3749bf6652fc1504b8676f
Jacob Hoopes <jake.hoopes@gmail.com>
2017-12-07 10:22:42 -0700

    Packer - add firefox provisioner to pull in firefox latest in order to support headless mode in CSI::Plugins::TransparentBrowser #bugfix

--- packer/provisioners/firefox.sh
+++ packer/provisioners/firefox.sh
@@ -2,6 +2,6 @@
 printf "Installing Firefox ********************************************************************"
 debian_unstable_sources='/etc/apt/sources.list.d/debian-unstable.list'
 sudo /bin/bash --login -c "echo deb http://ftp.debian.org/debian unstable main contrib non-free > ${debian_unstable_sources}"
-sudo /bin/bash ---login  -c "echo deb deb http://deb.debian.org/debian experimental main >> ${debian_unstable_sources}"
+sudo /bin/bash --login  -c "echo deb deb http://deb.debian.org/debian experimental main >> ${debian_unstable_sources}"
 sudo apt update
 sudo apt install -y firefox
