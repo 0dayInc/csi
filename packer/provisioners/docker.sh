e5d746f77a0a78729929a44132adfa181f3e1997
Jacob Hoopes <jake.hoopes@gmail.com>
2018-01-03 12:08:15 -0700

    Packer - bugfix in docker-ce provisioner

--- packer/provisioners/docker.sh
+++ packer/provisioners/docker.sh
@@ -4,7 +4,7 @@ docker_sources='/etc/apt/sources.list.d/docker.list'
 sudo /bin/bash --login -c "apt-get remove -y docker docker-engine docker.io*"
 sudo /bin/bash --login -c "apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common"
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo bash --login -c "echo 'deb https://download.docker.com/linux/debian stretch stable' > ${docker_sources}"
+sudo bash --login -c "echo 'deb [arch=amd64] https://download.docker.com/linux/debian stretch stable' > ${docker_sources}"
 sudo apt update
 sudo apt install -y docker-ce docker-compose
 sudo usermod -aG docker vagrant
