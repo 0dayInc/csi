1f0ea361feb7343af3134e79a9f6a14625d17f38
Jacob Hoopes <jake.hoopes@gmail.com>
2018-01-19 14:30:53 -0700

    Anisble - confirmation of transition into Ansible packing of VagrantCloud providers using Packer #rubocop_fixes

--- packer/ansible/roles/phantomjs/files/phantomjs.rb
+++ packer/ansible/roles/phantomjs/files/phantomjs.rb
@@ -19,9 +19,7 @@ links.each do |link|
   end
 end
 
-unless File.exist?(phantomjs_tar_bz2)
-  puts `sudo wget -O #{phantomjs_tar_bz2} #{phantomjs_linux_download}`
-end
+puts `sudo wget -O #{phantomjs_tar_bz2} #{phantomjs_linux_download}` unless File.exist?(phantomjs_tar_bz2)
 
 puts `sudo mkdir -p #{phantomjs_tar_extraction_path}` unless Dir.exist?(phantomjs_tar_extraction_path)
 puts `sudo tar -xjvf #{phantomjs_tar_bz2} -C #{phantomjs_tar_extraction_path}`
