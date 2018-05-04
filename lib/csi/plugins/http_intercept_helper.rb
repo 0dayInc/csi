763bb713721685b8476c9a763188df2f3eeae860
Jacob Hoopes <jake.hoopes@gmail.com>
2018-05-04 14:08:25 -0600

    CSI::Plugins::HTTPInterceptHelper - add #hash_to_raw method

--- lib/csi/plugins/http_intercept_helper.rb
+++ lib/csi/plugins/http_intercept_helper.rb
@@ -7,18 +7,18 @@ module CSI
       @@logger = CSI::Plugins::CSILogger.create
 
       # Supported Method Parameters::
-      # request_hash = CSI::Plugins::HTTPInterceptHelper.parse(
-      #   request: 'required = raw http request string to parse'
+      # request_hash = CSI::Plugins::HTTPInterceptHelper.raw_to_hash(
+      #   request_raw: 'required => raw http request string to convert to hash'
       # )
 
       public
 
-      def self.parse(opts = {})
-        request = opts[:request].to_s
+      def self.raw_to_hash(opts = {})
+        request_raw = opts[:request_raw].to_s
         request_hash = {}
 
         # Basic Parsing Begins
-        raw_intercepted_request_arr = request.split("\r\n")
+        raw_intercepted_request_arr = request_raw.split("\r\n")
 
         # Parse HTTP Protocol Request Line
         raw_request_line_arr = raw_intercepted_request_arr[0].split("\s")
@@ -61,7 +61,7 @@ module CSI
           raw_intercepted_request_arr[1..-1].each_with_index do |val, index|
             next if val != ''
             break_index = index + 2
-            request_hash[:http_body] = raw_intercepted_request_arr[break_index..-1]
+            request_hash[:http_body] = raw_intercepted_request_arr[break_index..-1].join(',')
           end
         when :PUT
           puts request_hash[:http_method]
@@ -72,6 +72,35 @@ module CSI
         end
 
         return request_hash
+      rescue => e
+        return e
+      end
+
+      # Supported Method Parameters::
+      # request_raw = CSI::Plugins::HTTPInterceptHelper.hash_to_raw(
+      #   request_hash: 'required => request_hash object returned by #raw_to_hash method'
+      # )
+
+      public
+
+      def self.hash_to_raw(opts = {})
+        request_hash = opts[:request_hash].to_s
+
+        request_raw = ''
+        request_raw = request_hash[:http_method]
+        request_raw = "#{request_raw}#{request_hash[:http_resource_path]}"
+        request_raw = "#{request_raw}#{request_hash[:http_version]}\r\n"
+        request_hash[:http_headers].each do |key, header_val|
+          request_raw = "#{request_raw}#{key}: #{header_val}\r\n"
+        end
+
+        if request_hash[:http_body] != ''
+          request_raw = "#{request_raw}#{request_hash[:http_body]}"
+        else
+          request_raw = "#{request_raw}#\r\n"
+        end
+
+        return request_raw
       rescue => e
         return e
       end
@@ -94,8 +123,12 @@ module CSI
 
       def self.help
         puts "USAGE:
-          request_hash = CSI::Plugins::HTTPInterceptHelper.parse(
-            request: 'required = raw http request string to parse'
+          request_hash = CSI::Plugins::HTTPInterceptHelper.raw_to_hash(
+            request_raw: 'required => raw http request string to convert to hash'
+          )
+
+          request_raw = CSI::Plugins::HTTPInterceptHelper.hash_to_raw(
+            request_hash: 'required => request_hash object returned by #raw_to_hash method'
           )
         "
       end
