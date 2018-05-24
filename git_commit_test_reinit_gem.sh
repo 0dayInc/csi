9bcb1ed03829aa74a37b1ef82386069ca8c13443
Jacob Hoopes <jake.hoopes@gmail.com>
2018-05-24 16:09:10 -0600

    git_commit_test_reinit_gem.sh - pull down latest to ensure latest version is incremented prior to running csi_autoinc_version

--- git_commit_test_reinit_gem.sh
+++ git_commit_test_reinit_gem.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if [[ $1 != "" && $2 != "" && $3 != "" ]]; then
+  git pull
   csi_autoinc_version
   git add . --all
   git commit -a -S --author="${1} <${2}>" -m "${3}"
--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.2.870'
+  VERSION = '0.2.871'
 end
