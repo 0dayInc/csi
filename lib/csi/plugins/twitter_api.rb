3246862833002f60bf1ba5af6b4350c604603cdf
Jacob Hoopes <jake.hoopes@gmail.com>
2018-06-13 14:27:44 -0600

    CSI::Plugins::TwitterAPI - Initial attempt @ working #login method

--- lib/csi/plugins/twitter_api.rb
+++ lib/csi/plugins/twitter_api.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'base64'
 require 'json'
 
 module CSI
@@ -10,71 +11,57 @@ module CSI
       @@logger = CSI::Plugins::CSILogger.create
 
       # Supported Method Parameters::
-      # authz_token = CSI::Plugins::TwitterAPI.login(
-      #   host: 'required - host/ip of TwitterAPI Server',
-      #   port: 'optional - port of TwitterAPI server (defaults to 8000)',
-      #   username: 'required - username to AuthN w/ api v2)',
-      #   password: 'optional - defect dojo api key (will prompt if nil)'
+      # bearer_token = CSI::Plugins::TwitterAPI.login(
+      #   access_token: 'required - access token for api requests on your own behalf',
+      #   access_token_secret: 'optional - access token secret (will prompt if nil)'
       # )
 
       public
 
       def self.login(opts = {})
-        http_body = {}
+        base_dd_api_uri = 'https://api.twitter.com'
 
-        host = opts[:host]
-        port = if opts[:port]
-                 opts[:port].to_i
-               else
-                 8000
-               end
-
-        http_body[:username] = opts[:username].to_s.scrub
-
-        if (host.include?('https://') && port == 443) || (host.include?('http://') && port == 80)
-          base_dd_api_uri = "#{host}/api/v2".to_s.scrub
-        else
-          base_dd_api_uri = "#{host}:#{port}/api/v2".to_s.scrub
-        end
-
-        http_body[:password] = if opts[:password].nil?
+        access_token = opts[:access_token].to_s.scrub
+        access_token_secret = if opts[:access_token_secret].nil?
                                  CSI::Plugins::AuthenticationHelper.mask_password
                                else
-                                 opts[:password].to_s.scrub
+                                 opts[:access_token_secret].to_s.scrub
                                end
 
+        authz_str = Base64.encode64("#{access_token}:#{access_token_secret}")
         http_headers = {}
         http_headers[:content_type] = 'application/json'
+        http_headers[:authorization] = "Basic #{authz_str}"
 
         @@logger.info("Logging into TwitterAPI REST API: #{base_dd_api_uri}")
         rest_client = CSI::Plugins::TransparentBrowser.open(browser_type: :rest)::Request
         response = rest_client.execute(
           method: :post,
-          url: "#{base_dd_api_uri}/api-token-auth/",
+          url: "#{base_dd_api_uri}/oauth2/token",
           verify_ssl: false,
           headers: http_headers,
-          payload: http_body.to_json
+          payload: 'grant_type=client_crdentials'
         )
 
         # Return array containing the post-authenticated TwitterAPI REST API token
         json_response = JSON.parse(response, symbolize_names: true)
-        authz_token = json_response[:token]
+        bearer_token = json_response[:access_token]
 
-        return authz_token
+        return bearer_token
       rescue => e
         raise e
       end
 
       # Supported Method Parameters::
       # twitter_rest_call(
-      #   authz_token: 'required authz_token returned from #login method',
+      #   bearer_token: 'required bearer_token returned from #login method',
       #   rest_call: 'required rest call to make per the schema',
       #   http_method: 'optional HTTP method (defaults to GET)
       #   http_body: 'optional HTTP body sent in HTTP methods that support it e.g. POST'
       # )
 
       private_class_method def self.twitter_rest_call(opts = {})
-        authz_token = opts[:authz_token]
+        bearer_token = opts[:bearer_token]
         rest_call = opts[:rest_call].to_s.scrub
         http_method = if opts[:http_method].nil?
                         :get
@@ -83,8 +70,8 @@ module CSI
                       end
         params = opts[:params]
         http_body = opts[:http_body].to_s.scrub
-        host = authz_token[:host]
-        port = authz_token[:port]
+        host = bearer_token[:host]
+        port = bearer_token[:port]
         base_zap_api_uri = "http://#{host}:#{port}"
 
         rest_client = CSI::Plugins::TransparentBrowser.open(browser_type: :rest)::Request
@@ -119,22 +106,22 @@ module CSI
 
         return response
       rescue StandardError, SystemExit, Interrupt => e
-        logout(authz_token) unless authz_token.nil?
+        logout(bearer_token) unless bearer_token.nil?
         raise e
       end
 
       # Supported Method Parameters::
       # CSI::Plugins::TwitterAPI.logout(
-      #   authz_token: 'required authz_token returned from #login method'
+      #   bearer_token: 'required bearer_token returned from #login method'
       # )
 
       public
 
       def self.logout(opts = {})
-        authz_token = opts[:authz_token]
+        bearer_token = opts[:bearer_token]
         @@logger.info('Logging out...')
         # TODO: Terminate Session if Possible via API Call
-        authz_token = nil
+        bearer_token = nil
       rescue => e
         raise e
       end
@@ -157,7 +144,7 @@ module CSI
 
       def self.help
         puts "USAGE:
-          authz_token = #{self}.login(
+          bearer_token = #{self}.login(
             host: 'required - host/ip of TwitterAPI Server',
             port: 'optional - port of TwitterAPI server (defaults to 8000)',
             username: 'required - username to AuthN w/ api v2)',
@@ -165,7 +152,7 @@ module CSI
           )
 
           #{self}.logout(
-            authz_token: 'required authz_token returned from #login method'
+            bearer_token: 'required bearer_token returned from #login method'
           )
 
           #{self}.authors
