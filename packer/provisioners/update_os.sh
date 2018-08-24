970e093d829d2497993aff88e4619380dfe30f6f
Jacob Hoopes <jake.hoopes@gmail.com>
2018-08-24 11:15:29 -0600

    Packer & Vagrant - bugfix in metasploit packer provisioner & run update_os provisioners in detached screens to handle future dpkg ncurses screens

--- packer/provisioners/update_os.sh
+++ packer/provisioners/update_os.sh
@@ -1,6 +1,15 @@
 #!/bin/bash --login
 # Automate Package Install Questions :)
 # Obtain values via: debconf-get-selections | less
-sudo /bin/bash --login -c "apt update && apt install -y debconf-utils && echo 'libc6 libraries/restart-without-asking boolean true' | debconf-set-selections && echo 'console-setup console-setup/codeset47 select Guess optimal character set' | debconf-set-selections && echo 'grub-pc grub-pc/install_devices multiselect /dev/sda1' | debconf-set-selections && echo 'wireshark-common wireshark-common/install-setuid boolean false' | debconf-set-selections && apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' dist-upgrade -y && apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' full-upgrade -y && apt autoremove -y && apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' install -y apt-file && apt-file update"
+sudo screen -S update_os -d -m /bin/bash --login -c "apt update && apt install -y debconf-utils && echo 'libc6 libraries/restart-without-asking boolean true' | debconf-set-selections && echo 'console-setup console-setup/codeset47 select Guess optimal character set' | debconf-set-selections && echo 'grub-pc grub-pc/install_devices multiselect /dev/sda1' | debconf-set-selections && echo 'wireshark-common wireshark-common/install-setuid boolean false' | debconf-set-selections && apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' dist-upgrade -y && apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' full-upgrade -y && apt autoremove -y && apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' install -y apt-file && apt-file update && apt install -y kali-linux-all"
 
-sudo screen -S update_os -d -m apt install -y kali-linux-all
+while true
+  # Wait until screen exits session
+  screen -ls
+  if [[ $? == 0 ]]; then
+    exit 0
+  else
+    printf '.'
+    sleep 30
+  end
+end
--- vagrant/provisioners/update_os.sh
+++ vagrant/provisioners/update_os.sh
@@ -2,3 +2,14 @@
 # Automate Package Install Questions :)
 # Obtain values via: debconf-get-selections | less
 sudo screen -S update_os -d -m /bin/bash --login -c "apt update && echo 'libc6 libraries/restart-without-asking boolean true' | debconf-set-selections && echo 'console-setup console-setup/codeset47 select Guess optimal character set' | debconf-set-selections && echo 'grub-pc grub-pc/install_devices multiselect /dev/sda1' | debconf-set-selections && echo 'wireshark-common wireshark-common/install-setuid boolean false' | debconf-set-selections && apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' dist-upgrade -y && apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' full-upgrade -y && apt autoremove -y && apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' install -y apt-file && apt-file update"
+
+while true
+  # Wait until screen exits session
+  screen -ls
+  if [[ $? == 0 ]]; then
+    exit 0
+  else
+    printf '.'
+    sleep 30
+  end
+end
