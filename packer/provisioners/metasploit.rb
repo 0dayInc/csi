6406a75c4039b50aaf454be91125f0b7af2a9b29
Jacob Hoopes <jake.hoopes@gmail.com>
2018-08-24 10:15:20 -0600

    Packer & Vagrant - bugfix in metasploit packer provisioner & run update_os provisioners in detached screens to handle future dpkg ncurses screens

--- packer/provisioners/metasploit.rb
+++ packer/provisioners/metasploit.rb
@@ -7,7 +7,7 @@ require 'yaml'
 printf 'Installing Metasploit *****************************************************************'
 metasploit_root = '/opt/metasploit-framework-dev'
 `sudo git clone https://github.com/rapid7/metasploit-framework.git #{metasploit_root}`
-`sudo apt install libpq-dev postgresql-server-dev-all`
+`sudo apt install -y libpq-dev postgresql-server-dev-all`
 metasploit_ruby_version = File.readlines("#{metasploit_root}/.ruby-version")[0].to_s.scrub.strip.chomp
 metasploit_gemset = File.readlines("#{metasploit_root}/.ruby-gemset")[0].to_s.scrub.strip.chomp
 `sudo bash --login -c "source /etc/profile.d/rvm.sh && rvm install ruby-#{metasploit_ruby_version} && rvm use ruby-#{metasploit_ruby_version} && rvm gemset create #{metasploit_gemset} && cd #{metasploit_root} && gem install bundler && bundle install"`
--- packer/provisioners/update_os.sh
+++ packer/provisioners/update_os.sh
@@ -9,4 +9,4 @@ sudo apt install -y debconf-utils
 # Obtain values via: debconf-get-selections | less
 sudo /bin/bash --login -c "echo 'libc6 libraries/restart-without-asking boolean true' | debconf-set-selections && echo 'console-setup console-setup/codeset47 select Guess optimal character set' | debconf-set-selections && echo 'grub-pc grub-pc/install_devices multiselect /dev/sda1' | debconf-set-selections && echo 'wireshark-common wireshark-common/install-setuid boolean false' | debconf-set-selections && apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' dist-upgrade -y && apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' full-upgrade -y && apt autoremove -y && apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' install -y apt-file && apt-file update"
 
-sudo apt install -y kali-linux-all
+sudo screen -S update_os -d -m apt install -y kali-linux-all
