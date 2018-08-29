98faf49015c49a0448cbe157b063372da570ca90
Jacob Hoopes <jake.hoopes@gmail.com>
2018-08-29 10:41:30 -0600

    Packer vim provsioner - update vim version

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.48'
+  VERSION = '0.3.49'
 end
--- packer/provisioners/vim.sh
+++ packer/provisioners/vim.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+sudo apt install -y vim
+
 # Disable auto-indent
-sudo sed -i 's/  filetype plugin indent on/  filetype plugin indent off/g' /usr/share/vim/vim80/defaults.vim
+sudo sed -i 's/  filetype plugin indent on/  filetype plugin indent off/g' /usr/share/vim/vim81/defaults.vim
 
 # Disable visual mode when highlighting text with mouse
-sudo sed -i 's/  set mouse=a/  set mouse-=a/g' /usr/share/vim/vim80/defaults.vim
+sudo sed -i 's/  set mouse=a/  set mouse-=a/g' /usr/share/vim/vim81/defaults.vim
