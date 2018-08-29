8722444d5a491c29ce1049abb28271b911835ea5
Jacob Hoopes <jake.hoopes@gmail.com>
2018-08-29 11:00:46 -0600

    Packer vim provsioner - skip defaults to avoid visula mode w/ mouse & auto-indent

--- packer/provisioners/vim.sh
+++ packer/provisioners/vim.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+default_vimrc='/usr/share/vim/vim81/defaults.vim'
+global_vimrc='/etc/vim/vimrc'
+
 sudo apt install -y vim
 
+sudo cp $global_vimrc $global_vimrc.BAK
+sudo cat $default_vimrc > $global_vimrc
 # Disable auto-indent
-#sudo sed -i 's/  filetype plugin indent on/  filetype plugin indent off/g' /usr/share/vim/vim81/defaults.vim
+sudo sed -i 's/  filetype plugin indent on/  filetype plugin indent off/g' $global_vimrc
 
 # Disable visual mode when highlighting text with mouse
-#sudo sed -i 's/  set mouse=a/  set mouse-=a/g' /usr/share/vim/vim81/defaults.vim
+sudo sed -i 's/  set mouse=a/  set mouse-=a/g' $global_vimrc
 
-sudo echo 'let skip_defaults_vim=1' >> /etc/vimrc
+# Ensure Global vimrc overrides default vimrc
+sudo echo 'let skip_defaults_vim=1' >> $global_vimrc
