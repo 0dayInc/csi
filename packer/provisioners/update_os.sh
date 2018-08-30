e3724f5844a0bfa52e88657a0336809921c1d2e5
Jacob Hoopes <jake.hoopes@gmail.com>
2018-08-30 10:44:39 -0600

    Packer update_os.sh - add temporary openssl version pin to ensure csi_arachni_rest can work while proxying REST api requests through the arachni proxy plugin Arachni/arachni#1011

--- packer/provisioners/update_os.sh
+++ packer/provisioners/update_os.sh
@@ -1,6 +1,11 @@
 #!/bin/bash --login
 # Automate Package Install Questions :)
 # Obtain values via: debconf-get-selections | less
+
+# This is a temporary fix to support arachni scanning
+# until it can be addressed in https://github.com/Arachni/arachni/issues/1011
+sudo echo -e "Package: openssl\nPin: version 1.1.0*\nPin-Priority: 1001" > /etc/preferences.d/openssl
+
 sudo screen -L -S update_os -d -m /bin/bash --login -c "apt update && apt install -y debconf-utils && echo 'samba-common samba-common/dhcp boolean false' | debconf-set-selections && echo 'libc6 libraries/restart-without-asking boolean true' | debconf-set-selections && echo 'console-setup console-setup/codeset47 select Guess optimal character set' | debconf-set-selections && echo 'grub-pc grub-pc/install_devices multiselect /dev/sda1' | debconf-set-selections && echo 'wireshark-common wireshark-common/install-setuid boolean false' | debconf-set-selections && apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' dist-upgrade -y && apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' full-upgrade -y && apt autoremove -y && apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' install -y apt-file && apt-file update && apt install -y kali-linux-all"
 
 while true; do
