ee2b4665396718a97f3728acf89713d272fa368a
Jacob Hoopes <jake.hoopes@gmail.com>
2018-09-05 11:15:27 -0600

    Packer update_os.sh provisioner - rewrite for better debugging error mitigation techniques, etc #bugfix in update_os instructions

--- packer/provisioners/update_os.sh
+++ packer/provisioners/update_os.sh
@@ -2,9 +2,6 @@
 # Automate Package Install Questions :)
 # Obtain values via: debconf-get-selections | less
 
-# This is a temporary fix to support arachni scanning
-# until it can be addressed in https://github.com/Arachni/arachni/issues/1011
-
 grok_error() {
   while true; do
     # Wait until screen exits session
@@ -16,6 +13,7 @@ grok_error() {
         cat screenlog.*
         exit 1
       else
+        echo 'No errors in update detected...moving onto the next.'
         break 
       fi
     else
@@ -25,33 +23,51 @@ grok_error() {
   done
 }
 
-
 # PINNED PACKAGES
 # pin openssl for arachni proxy plugin Arachni/arachni#1011
 sudo echo "Package: openssl" > /etc/apt/preferences.d/openssl
 sudo echo "Pin: version 1.1.0*" >> /etc/apt/preferences.d/openssl
 sudo echo "Pin-Priority: 1001" >> /etc/apt/preferences.d/openssl
 
-update_os_instructions=(
-  "apt update"
-  "apt install -y debconf-utils"
-  "echo 'samba-common samba-common/dhcp boolean false' | debconf-set-selections"
-  "echo 'libc6 libraries/restart-without-asking boolean true' | debconf-set-selections"
-  "echo 'console-setup console-setup/codeset47 select Guess optimal character set' | debconf-set-selection"
-  "echo 'grub-pc grub-pc/install_devices multiselect /dev/sda1' | debconf-set-selections"
-  "echo 'wireshark-common wireshark-common/install-setuid boolean false' | debconf-set-selections"
-  "apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' dist-upgrade -y"
-  "apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' full-upgrade -y"
-  "apt autoremove -y"
-  "apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' install -y apt-file"
-  "apt-file update"
-  "apt install -y kali-linux-all"
-)
-
 # Update OS per update_os_instructions function and grok for errors in screen session logs
 # to mitigate introduction of bugs during updgrades.
-for instruction in ${update_os_instructions[@]} ; do
-  screen_cmd="sudo screen -L -S update_os -d -m /bin/bash --login -c \"${instruction}\""
-  $screen_cmd
-  grok_error
-done
+screen_cmd='sudo screen -L -S update_os -d -m /bin/bash --login -c'
+
+$screen_cmd "apt update"
+grok_error
+
+$screen_cmd "apt install -y debconf-utils"
+grok_error
+
+$screen_cmd "echo 'samba-common samba-common/dhcp boolean false' | debconf-set-selections"
+grok_error
+
+$screen_cmd "echo 'libc6 libraries/restart-without-asking boolean true' | debconf-set-selections"
+grok_error
+
+$screen_cmd "echo 'console-setup console-setup/codeset47 select Guess optimal character set' | debconf-set-selection"
+grok_error
+
+$screen_cmd "echo 'grub-pc grub-pc/install_devices multiselect /dev/sda1' | debconf-set-selections"
+grok_error
+
+$screen_cmd "echo 'wireshark-common wireshark-common/install-setuid boolean false' | debconf-set-selections"
+grok_error
+
+$screen_cmd "apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' dist-upgrade -y"
+grok_error
+
+$screen_cmd "apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' full-upgrade -y"
+grok_error
+
+$screen_cmd "apt autoremove -y"
+grok_error
+
+$screen_cmd "apt -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' install -y apt-file"
+grok_error
+
+$screen_cmd "apt-file update"
+grok_error
+
+$screen_cmd "apt install -y kali-linux-all"
+grok_error
