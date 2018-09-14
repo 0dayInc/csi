ced2a34af1a324a770d32d68702c039452b503cd
Jacob Hoopes <jake.hoopes@gmail.com>
2018-09-14 12:50:24 -0600

    Packer - fix race condition on permissions of authorized_keys file

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.102'
+  VERSION = '0.3.103'
 end
--- packer/provisioners/install_vagrant_ssh_key.sh
+++ packer/provisioners/install_vagrant_ssh_key.sh
@@ -1,8 +1,9 @@
 #!/bin/bash --login
 mkdir -p /home/vagrant/.ssh
 chmod 0700 /home/vagrant/.ssh
+touch /home/vagrant/.ssh/authorized_keys
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /home/vagrant/.ssh
 wget \
   --no-check-certificate https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub \
   -O /home/vagrant/.ssh/authorized_keys
-chmod 0600 /home/vagrant/.ssh/authorized_keys
-chown -R vagrant /home/vagrant/.ssh
