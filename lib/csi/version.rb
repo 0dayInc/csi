4a972683886ae62e1a305bf721b755aeac8220a7
Jacob Hoopes <jake.hoopes@gmail.com>
2018-10-01 16:14:11 -0600

    Packer - update source of insecure ssh public key for vagrant

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.126'
+  VERSION = '0.3.127'
 end
--- packer/provisioners/install_vagrant_ssh_key.sh
+++ packer/provisioners/install_vagrant_ssh_key.sh
@@ -5,5 +5,5 @@ touch /home/vagrant/.ssh/authorized_keys
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:vagrant /home/vagrant/.ssh
 wget \
-  --no-check-certificate https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub \
+  --no-check-certificate https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub \
   -O /home/vagrant/.ssh/authorized_keys
