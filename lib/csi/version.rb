ac0d1df56ba07b478d3c452fd0f154d041393b1a
Jacob Hoopes <jake.hoopes@gmail.com>
2018-10-31 14:04:48 -0600

    Packer update_os.sh provisioner - no dpkg screen on obsolete version of postgres #add_grok_error

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.144'
+  VERSION = '0.3.145'
 end
--- packer/provisioners/update_os.sh
+++ packer/provisioners/update_os.sh
@@ -49,6 +49,7 @@ $screen_cmd "apt install -y debconf-utils ${assess_update_errors}"
 grok_error
 
 $screen_cmd "echo 'postgresql-common postgresql-common/obsolete-major boolean true' | ${debconf_set} ${assess_update_errors}"
+grok_error
 
 $screen_cmd "echo 'samba-common samba-common/dhcp boolean false' | ${debconf_set} ${assess_update_errors}"
 grok_error
