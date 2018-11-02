abc27618bd700188f5cfdec6329f5a7639eb1cf8
Jacob Hoopes <jake.hoopes@gmail.com>
2018-11-02 08:56:48 -0600

    Packer - update geckodriver.sh to avoid having to maintain versions of geckodriver and store in thir_party folder #eyewitness_installs_geckodriver_for_us

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.151'
+  VERSION = '0.3.152'
 end
--- packer/provisioners/geckodriver.sh
+++ packer/provisioners/geckodriver.sh
@@ -1,3 +1,5 @@
 #!/bin/bash --login
-firefox_plugin_path='/csi/third_party/geckodriver-v0.19.1-linux64.tar.gz'
-sudo tar -xzvf $firefox_plugin_path -C /usr/local/bin
+#firefox_plugin_path='/csi/third_party/geckodriver-v0.19.1-linux64.tar.gz'
+#sudo tar -xzvf $firefox_plugin_path -C /usr/local/bin
+# Preferred way over commented cmds above since eyewitness includes geckodriver :)
+sudo apt install -y eyewitness
