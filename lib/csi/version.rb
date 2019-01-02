452b89bf6e21934ebde7be09497a133e6840b9e1
Jacob Hoopes <jake.hoopes@gmail.com>
2019-01-02 14:10:52 -0700

    Packer ssllabs-scan.sh Provisioner - update symlink source

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.219'
+  VERSION = '0.3.220'
 end
--- packer/provisioners/ssllabs-scan.sh
+++ packer/provisioners/ssllabs-scan.sh
@@ -2,4 +2,4 @@
 ssllabs_root="/opt/ssllabs-scan"
 sudo apt install -y golang
 sudo /bin/bash --login -c "cd /opt && git clone https://github.com/ssllabs/ssllabs-scan.git"
-sudo /bin/bash --login -c "cd ${ssllabs_root} && make && ln -sf ${ssllabs_root}/ssllabs-scan /usr/local/bin/"
+sudo /bin/bash --login -c "cd ${ssllabs_root} && make && ln -sf ${ssllabs_root}/ssllabs-scan-v3 /usr/local/bin/ssllabs-scan"
