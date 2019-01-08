947f6827c4850f3394322b23aecf8ca0ae6b530d
Jacob Hoopes <jake.hoopes@gmail.com>
2019-01-08 15:33:20 -0700

    Packer rvm.sh - add new key to the mix

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.233'
+  VERSION = '0.3.234'
 end
--- packer/provisioners/rvm.sh
+++ packer/provisioners/rvm.sh
@@ -15,6 +15,7 @@ esac
 
 curl -sSL https://rvm.io/mpapis.asc | sudo gpg2 --no-tty --import -
 sudo /bin/bash --login -c 'echo -e "trust\n5\ny\n" | gpg2 --no-tty --command-fd 0 --edit-key 409B6B1796C275462A1703113804BB82D39DC0E3'
+sudo /bin/bash --login -c 'echo -e "trust\n5\ny\n" | gpg2 --no-tty --command-fd 0 --edit-key 7D2BAF1CF37B13E2069D6956105BD0E739499BDB'
 
 # Multi-user install required due to the need to run MSFRPCD as root w/in metasploit gemset
 curl -sSL https://get.rvm.io | sudo bash -s latest
