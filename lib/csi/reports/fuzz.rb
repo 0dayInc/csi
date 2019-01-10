39bab68e371a4b92be34dd6527977e1f3687074d
Jacob Hoopes <jake.hoopes@gmail.com>
2019-01-10 10:17:41 -0700

    CSI::Reports::Fuzz - encode column data

--- lib/csi/reports/fuzz.rb
+++ lib/csi/reports/fuzz.rb
@@ -154,35 +154,35 @@ module CSI
                       "data": "timestamp",
                       "width": "200px",
                       "render": function (data, type, row, meta) {
-                        return '<tr><td style="width:200px;" align="left">' + data.render.text() + '</td>';
+                        return '<tr><td style="width:200px;" align="left">' + data + '</td>';
                       }
                     },
                     {
                       "data": "request",
                       "width": "200px",
                       "render": function (data, type, row, meta) {
-                        return '<tr><td style="width:200px;" align="left">' + data.render.text() + '</td>';
+                        return '<tr><td style="width:200px;" align="left">' + data + '</td>';
                       }
                     },
                     {
                       "data": "request_len",
                       "width": "200px",
                       "render": function (data, type, row, meta) {
-                        return '<tr><td style="width:200px;" align="left">' + data.render.text() + '</td>';
+                        return '<tr><td style="width:200px;" align="left">' + data + '</td>';
                       }
                     },
                     {
                       "data": "response",
                       "width": "200px",
                       "render": function (data, type, row, meta) {
-                        return '<tr><td style="width:200px;" align="left">' + data.render.text() + '</td>';
+                        return '<tr><td style="width:200px;" align="left">' + data + '</td>';
                       }
                     },
                     {
                       "data": "response_len",
                       "width": "200px",
                       "render": function (data, type, row, meta) {
-                        return '<tr><td style="width:200px;" align="left">' + data.render.text() + '</td>';
+                        return '<tr><td style="width:200px;" align="left">' + data + '</td>';
                       }
                     }
                   ]
--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.309'
+  VERSION = '0.3.310'
 end
