3272a1ae93f1414103875063cfd6cb116449c6d7
Jacob Hoopes <jake.hoopes@gmail.com>
2019-01-10 16:27:32 -0700

    Packer rvm.sh Provisioner - update keys per https://rvm.io/rvm/security

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.317'
+  VERSION = '0.3.318'
 end
--- packer/provisioners/rvm.sh
+++ packer/provisioners/rvm.sh
@@ -14,9 +14,12 @@ case $os in
 esac
 
 #curl -sSL https://rvm.io/mpapis.asc | sudo gpg2 --no-tty --import -
-sudo /bin/bash --login -c 'gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB'
-sudo /bin/bash --login -c 'echo -e "trust\n5\ny\n" | gpg2 --no-tty --command-fd 0 --edit-key 409B6B1796C275462A1703113804BB82D39DC0E3'
-sudo /bin/bash --login -c 'echo -e "trust\n5\ny\n" | gpg2 --no-tty --command-fd 0 --edit-key 7D2BAF1CF37B13E2069D6956105BD0E739499BDB'
+key1='409B6B1796C275462A1703113804BB82D39DC0E3'
+key2='7D2BAF1CF37B13E2069D6956105BD0E739499BDB'
+
+sudo /bin/bash --login -c "gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys ${key1} ${key2}"
+sudo /bin/bash --login -c "echo -e "trust\n5\ny\n" | gpg2 --no-tty --command-fd 0 --edit-key ${key1}"
+sudo /bin/bash --login -c "echo -e "trust\n5\ny\n" | gpg2 --no-tty --command-fd 0 --edit-key ${key2}"
 
 # Multi-user install required due to the need to run MSFRPCD as root w/in metasploit gemset
 curl -sSL https://get.rvm.io | sudo bash -s latest
