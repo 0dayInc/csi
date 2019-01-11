69b5be70a6d13319adb2afc24ecbaa2239101cee
Jacob Hoopes <jake.hoopes@gmail.com>
2019-01-11 10:59:22 -0700

    Packer rvm.sh Provisioner - update keys per https://rvm.io/rvm/security

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.326'
+  VERSION = '0.3.327'
 end
--- packer/provisioners/rvm.sh
+++ packer/provisioners/rvm.sh
@@ -17,7 +17,9 @@ esac
 key1='409B6B1796C275462A1703113804BB82D39DC0E3'
 key2='7D2BAF1CF37B13E2069D6956105BD0E739499BDB'
 
-sudo /bin/bash --login -c "gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys ${key1} ${key2}"
+# sudo /bin/bash --login -c "gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys ${key1} ${key2}"
+sudo /bin/bash --login -c 'curl -sSL https://rvm.io/mpapis.asc | sudo gpg2 --import -'
+sudo /bin/bash --login -c 'curl -sSL https://rvm.io/pkuczynski.asc | sudo gpg2 --import -'
 sudo /bin/bash --login -c "echo -e "trust\n5\ny\n" | gpg2 --no-tty --command-fd 0 --edit-key ${key1}"
 sudo /bin/bash --login -c "echo -e "trust\n5\ny\n" | gpg2 --no-tty --command-fd 0 --edit-key ${key2}"
 
