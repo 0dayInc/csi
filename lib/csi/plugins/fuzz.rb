d5ee586ff604c88ef134f55b455063c620466f5f
Jacob Hoopes <jake.hoopes@gmail.com>
2019-01-16 14:46:38 -0700

    CSI::Plugins::Fuzz plugin - catch Errno::ECONNRESET && EINVAL

--- lib/csi/plugins/fuzz.rb
+++ lib/csi/plugins/fuzz.rb
@@ -103,12 +103,13 @@ module CSI
 
         return socket_fuzz_results_arr
       rescue Errno::ECONNRESET, Errno::EINVAL => e
+        response = e.message.to_s
         this_socket_fuzz_result = {}
         this_socket_fuzz_result[:timestamp] = Time.now.strftime('%Y-%m-%d %H:%M:%S.%9N %z').to_s
         this_socket_fuzz_result[:request] = this_request.to_s.inspect
         this_socket_fuzz_result[:request_len] = this_request.length
-        this_socket_fuzz_result[:response] = e
-        this_socket_fuzz_result[:response_len] = 0
+        this_socket_fuzz_result[:response] = response
+        this_socket_fuzz_result[:response_len] = response.length
         socket_fuzz_results_arr.push(this_socket_fuzz_result)
         return socket_fuzz_results_arr
       rescue => e
--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.368'
+  VERSION = '0.3.369'
 end
