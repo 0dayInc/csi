c3ed5185523bb4ab9c2b69cee7001118569e3006
Jacob Hoopes <jake.hoopes@gmail.com>
2019-01-21 15:06:25 -0700

    Gemfile & build_csi_gem.sh - reintroduce bundler-audit

--- build_csi_gem.sh
+++ build_csi_gem.sh
@@ -10,10 +10,9 @@ if [[ $old_ruby_version == $new_ruby_version ]]; then
   rake rerdoc
   gem rdoc --rdoc --ri --overwrite -V csi
   bundle update
-  # TODO: Uncomment once bundler-audit gem in Gemfile > 0.6.0
-  # bundle-audit update
-  # echo "Invoking bundle-audit Gemfile Scanner..."
-  # bundle-audit
+  bundle-audit update
+  echo "Invoking bundle-audit Gemfile Scanner..."
+  bundle-audit
 else
   cd /csi && ./upgrade_ruby.sh $new_ruby_version $old_ruby_version
 fi
