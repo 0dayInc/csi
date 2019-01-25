38699f88b4ec84323c23d4636e8feec2657ac993
Jacob Hoopes <jake.hoopes@gmail.com>
2019-01-25 12:06:59 -0700

    Migrate csi_autoinc_version to .travis.yml #rollback

--- git_commit_test_reinit_gem.sh
+++ git_commit_test_reinit_gem.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ $1 != "" && $2 != "" && $3 != "" ]]; then
   git pull
-  # csi_autoinc_version
+  csi_autoinc_version
   git add . --all
   git commit -a -S --author="${1} <${2}>" -m "${3}"
   ./build_csi_gem.sh
--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.471'
+  VERSION = '0.3.472'
 end
