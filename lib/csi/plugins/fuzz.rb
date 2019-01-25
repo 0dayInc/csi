1824ffe92dfeda278be57e518a8ab74cc67346fd
Jacob Hoopes <jake.hoopes@gmail.com>
2019-01-25 15:09:17 -0700

    CSI::Plugins::Fuzz - another attempt at sending bytes to target socket in their true form

--- lib/csi/plugins/fuzz.rb
+++ lib/csi/plugins/fuzz.rb
@@ -105,7 +105,7 @@ module CSI
             this_socket_fuzz_result[:request_len] = this_request.length
 
             # Send Fuzz Payload
-            sock_obj.write("#{this_request}")
+            sock_obj.write(this_request)
 
             does_respond = IO.select([sock_obj], nil, nil, response_timeout)
             if does_respond
