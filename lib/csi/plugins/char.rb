2ead6b29552985e5a153a1e74d5b212c67f4544a
Jacob Hoopes <jake.hoopes@gmail.com>
2019-01-25 22:46:40 -0700

    CSI::Plugins::Char module - minor tweak

--- lib/csi/plugins/char.rb
+++ lib/csi/plugins/char.rb
@@ -57,6 +57,7 @@ module CSI
           encoder_list_arr.each do |encoder|
             this_encoder_key = encoder.downcase.tr('-', '_').to_sym
             begin
+              # char_hash[this_encoder_key] = this_utf8.force_encoding(encoder)
               char_hash[this_encoder_key] = this_utf8.encode(encoder, 'UTF-8')
             rescue
               char_hash[this_encoder_key] = nil
@@ -366,6 +367,7 @@ module CSI
             next
           end
         end
+        print "\n"
       rescue => e
         raise e
       end
