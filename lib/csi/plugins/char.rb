f2b761d7e07d3c57032e278e26d1efd9b264bcfb
Jacob Hoopes <jake.hoopes@gmail.com>
2019-01-27 13:03:30 -0700

    CSI::Plugins::Char - improved empty file cleanup in #generate_encoded_files

--- lib/csi/plugins/char.rb
+++ lib/csi/plugins/char.rb
@@ -386,6 +386,7 @@ module CSI
           rescue => e
             puts "FILE GENERATION ATTEMPT OF: #{this_file} RESULTED THE FOLLOWING ERROR:"
             puts "#{e.class}: #{e.message}\n#{e.backtrace}\n\n\n"
+            File.unlink(this_file) if File.read(this_file).length.zero?
             next
           end
         end
