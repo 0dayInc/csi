e4cc60437feea9e642bd80dade6a6f63a063b161
Jacob Hoopes <jake.hoopes@gmail.com>
2019-02-04 12:47:37 -0700

    CSI::Plugins::Fuzz module - remove #undump method in sock_obj.write w/in #socket method

--- lib/csi/plugins/fuzz.rb
+++ lib/csi/plugins/fuzz.rb
@@ -111,7 +111,8 @@ module CSI
             this_socket_fuzz_result[:request_len] = this_request.length
 
             # Send Fuzz Payload in its rawest form
-            sock_obj.write(this_request.undump)
+            # sock_obj.write(this_request.undump)
+            sock_obj.write(this_request)
 
             does_respond = IO.select([sock_obj], nil, nil, response_timeout)
             if does_respond
