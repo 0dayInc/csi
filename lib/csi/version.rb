7892924ec60f936f186b11e780b5294be5b1b5b5
Jacob Hoopes <jake.hoopes@gmail.com>
2019-06-17 10:24:16 -0600

    Vagrant - bugfix in csi provisioner

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.623'
+  VERSION = '0.3.624'
 end
--- vagrant/provisioners/csi.sh
+++ vagrant/provisioners/csi.sh
@@ -1,2 +1,3 @@
 #!/bin/bash --login
-sudo /bin/bash --login -c 'cd /csi && ./reinstall_csi_gemset.sh; ./build_csi_gem.sh'
+sudo /bin/bash --login -c 'cd /csi && ./reinstall_csi_gemset.sh'
+sudo /bin/bash --login -c 'cd /csi && ./build_csi_gem.sh'
