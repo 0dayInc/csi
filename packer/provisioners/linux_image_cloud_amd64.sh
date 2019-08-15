b89fc8602e55b59060d9c350e1042872a90787af
Jacob Hoopes <jake.hoopes@gmail.com>
2019-08-15 15:39:24 -0600

    Vagrantfile provisioners/linux_image_cloud_amd64.sh - more switchy

--- packer/provisioners/linux_image_cloud_amd64.sh
+++ packer/provisioners/linux_image_cloud_amd64.sh
@@ -1,4 +1,4 @@
 #!/bin/bash --login
-sudo apt install -y linux-headers-cloud-amd64
 sudo apt install -y linux-image-cloud-amd64
 sudo update-initramfs -u
+sudo apt install -y linux-headers-cloud-amd64
