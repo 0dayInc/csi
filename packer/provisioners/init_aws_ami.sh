768813e3a3bcbaec44389a0c76b8812c6943a8bb
Jacob Hoopes <jake.hoopes@gmail.com>
2019-08-16 11:29:30 -0600

    Packer change csi_admin for intial Kali Rolling AMI to ec2-user and implement a provisioner to create the default csi_admin account

---
+++ packer/provisioners/init_aws_ami.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+hostname=`hostname`
+csi_admin_user='csi_admin'
+
+sudo useradd -m $csi_admin_user
+sudo usermod -aG sudo $csi_admin_user
+echo "Updating FQDN: ${hostname}"
+cat /etc/hosts | grep "${hostname}"
+sudo sed "s/127.0.0.1/127.0.0.1 ${hostname}/g" -i /etc/hosts
+sudo sed -i -e 's/^Defaults.*requiretty/# Defaults requiretty/g' /etc/sudoers
+echo 'Defaults:csi_admin !requiretty' >> /etc/sudoers
+sudo userdel ec2-user
