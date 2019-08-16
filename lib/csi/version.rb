c9e2c5b826776539f569737659bb71d02f2731cc
Jacob Hoopes <jake.hoopes@gmail.com>
2019-08-16 14:30:16 -0600

    Packer - minor bugfix in provisioners/init_aws_ami.sh

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.664'
+  VERSION = '0.3.665'
 end
--- packer/provisioners/install_vagrant_ssh_key.sh
+++ packer/provisioners/install_vagrant_ssh_key.sh
@@ -1,9 +1,9 @@
 #!/bin/bash --login
-mkdir -p /home/csi_admin/.ssh
-chmod 0700 /home/csi_admin/.ssh
-touch /home/csi_admin/.ssh/authorized_keys
-chmod 0600 /home/csi_admin/.ssh/authorized_keys
-chown -R csi_admin:csi_admin /home/csi_admin/.ssh
-wget \
+sudo mkdir -p /home/csi_admin/.ssh
+sudo chmod 0700 /home/csi_admin/.ssh
+sudo touch /home/csi_admin/.ssh/authorized_keys
+sudo chmod 0600 /home/csi_admin/.ssh/authorized_keys
+sudo chown -R csi_admin:csi_admin /home/csi_admin/.ssh
+sudo wget \
   --no-check-certificate https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub \
   -O /home/csi_admin/.ssh/authorized_keys
