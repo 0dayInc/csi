972a366ead36a22c20b1cdc6d83c5fc8abd28429
Jacob Hoopes <jake.hoopes@gmail.com>
2019-08-19 09:11:47 -0600

    Packer - minor bugfix in peda provisioner

--- packer/provisioners/peda.sh
+++ packer/provisioners/peda.sh
@@ -1,4 +1,4 @@
 #!/bin/bash --login
 # PEDA - Python Exploit Development Assistance for GDB to be used w/ AFL
 sudo /bin/bash --login -c 'cd /opt && git clone https://github.com/longld/peda.git peda-dev && echo "source /opt/peda-dev/peda.py" >> /root/.gdbinit'
-sudo echo "source /opt/peda-dev/peda.py" >> /home/csi_admin/.gdbinit
+sudo -H -u csi_admin /bin/bash --login -c 'echo "source /opt/peda-dev/peda.py" >> /home/csi_admin/.gdbinit'
