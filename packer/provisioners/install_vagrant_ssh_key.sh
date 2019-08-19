5a0e1b4936624773e8f7d511b0d3555017cb45e9
Jacob Hoopes <jake.hoopes@gmail.com>
2019-08-19 16:15:53 -0600

    Packer & Vagrant - tweaks to begin supporting CSI Kali Rolling AMI

--- packer/provisioners/install_vagrant_ssh_key.sh
+++ packer/provisioners/install_vagrant_ssh_key.sh
@@ -7,6 +7,4 @@ if [[ ! -d '/home/csi_admin/.ssh' ]]; then
   sudo chown -R csi_admin:csi_admin /home/csi_admin/.ssh
 fi
 
-sudo wget \
-  --no-check-certificate https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub \
-  -O ->> /home/csi_admin/.ssh/authorized_keys
+sudo /bin/bash --login -c 'wget --no-check-certificate https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub -O ->> /home/csi_admin/.ssh/authorized_keys'
