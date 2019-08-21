afcb9c4dc7f54c819e8a26a0e093b686a3bfaebe
Jacob Hoopes <jake.hoopes@gmail.com>
2019-08-21 11:26:09 -0600

    CSI Framework - update username from csi_admin to csiadmin

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.675'
+  VERSION = '0.3.676'
 end
--- packer/ansible/roles/install_vagrant_ssh_key/files/install_vagrant_ssh_key.sh
+++ packer/ansible/roles/install_vagrant_ssh_key/files/install_vagrant_ssh_key.sh
@@ -1,8 +1,8 @@
 #!/bin/bash --login
-mkdir -p /home/csi_admin/.ssh
-chmod 0700 /home/csi_admin/.ssh
+mkdir -p /home/csiadmin/.ssh
+chmod 0700 /home/csiadmin/.ssh
 wget \
   --no-check-certificate https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub \
-  -O /home/csi_admin/.ssh/authorized_keys
-chmod 0600 /home/csi_admin/.ssh/authorized_keys
-chown -R csi_admin:csi_admin /home/csi_admin/.ssh
+  -O /home/csiadmin/.ssh/authorized_keys
+chmod 0600 /home/csiadmin/.ssh/authorized_keys
+chown -R csiadmin:csiadmin /home/csiadmin/.ssh
