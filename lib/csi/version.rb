9cb455f17f758fa9cf7c9a5bb239a93d7f53c6b5
Jacob Hoopes <jake.hoopes@gmail.com>
2019-08-21 16:53:34 -0600

    Packer - remove pinned kernel file and remove old aws ami provisioner

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.680'
+  VERSION = '0.3.681'
 end
--- packer/provisioners/post_install.sh
+++ packer/provisioners/post_install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash --login
+pinned_kernel_file='/etc/apt/preferences.d/linux-image-amd64'
+
 # Change password at first login
 sudo passwd --expire csiadmin
 
 # Remove Packer SSH Key from authorized_keys file
 sudo sed -i '/packer/d' /home/csiadmin/.ssh/authorized_keys
+
+# Removed pinned kernel from AWS AMIs
+if [[ -f $pinned_kernel_file ]]; then
+  sudo rm $pinned_kernel_file
+fi
