029e19d4eae1edc6cb8733cb362e46addd4caddc
Jacob Hoopes <jake.hoopes@gmail.com>
2019-08-23 14:40:21 -0600

    Packer provisioner init_aws_ami.sh shorten variable names so all lines are same byte-length

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.686'
+  VERSION = '0.3.687'
 end
--- packer/provisioners/init_aws_ami.sh
+++ packer/provisioners/init_aws_ami.sh
@@ -1,10 +1,10 @@
 #!/bin/bash --login
-system_map='System.map-4.19.0-kali4-amd64'
+sysmap='System.map-4.19.0-kali4-amd64'
 config='config-4.19.0-kali4-amd64'
 initrd='initrd.img-4.19.0-kali4-amd64'
 kernel='vmlinuz-4.19.0-kali4-amd64'
 
-sudo /bin/bash --login -c "mv /tmp/${system_map} /boot && chmod 0644 /boot/${system_map} && chown root:root /boot/${system_map}"
+sudo /bin/bash --login -c "mv /tmp/${sysmap} /boot && chmod 0644 /boot/${sysmap} && chown root:root /boot/${sysmap}"
 sudo /bin/bash --login -c "mv /tmp/${config} /boot && chmod 0644 /boot/${config} && chown root:root /boot/${config}"
 sudo /bin/bash --login -c "mv /tmp/${initrd} /boot && chmod 0644 /boot/${initrd} && chown root:root /boot/${initrd}"
 sudo /bin/bash --login -c "mv /tmp/${kernel} /boot && chmod 0644 /boot/${kernel} && chown root:root /boot/${kernel}"
