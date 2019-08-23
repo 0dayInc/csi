6ecd9ebffc608a60e95471b95533864dda9143d7
Jacob Hoopes <jake.hoopes@gmail.com>
2019-08-23 14:16:49 -0600

    Packer - get proper kernel for vmimport conversion to AMI

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.684'
+  VERSION = '0.3.685'
 end
--- packer/provisioners/init_aws_ami.sh
+++ packer/provisioners/init_aws_ami.sh
@@ -1,50 +1,10 @@
 #!/bin/bash --login
-grok_error() {
-  while true; do
-    # Wait until screen exits session
-    sudo screen -ls | grep update_os
-    if [[ $? == 1 ]]; then
-      grep UPDATE_ABORT screenlog.*
-      if [[ $? == 0 ]]; then
-        echo 'Failures encountered in screenlog for update_os session!!!'
-        cat screenlog.*
-        exit 1
-      else
-        echo 'No errors in update detected...moving onto the next.'
-        break
-      fi
-    else
-      printf '.'
-      sleep 9
-    fi
-  done
-}
+system_map='/tmp/System.map-4.19.0-kali4-amd64'
+config='/tmp/config-4.19.0-kali4-amd64'
+initrd='/tmp/initrd.img-4.19.0-kali4-amd64'
+kernel='/tmp/vmlinuz-4.19.0-kali4-amd64'
 
-screen_cmd='sudo screen -L -S update_os -d -m /bin/bash --login -c'
-assess_update_errors='|| echo UPDATE_ABORT && exit 1'
-
-# Cleanup up prior screenlog.0 file from previous update_os failure(s)
-if [[ -e screenlog.0 ]]; then
-  sudo rm screenlog.*
-fi
-
-$screen_cmd "apt install -y grub-legacy ${assess_update_errors}"
-grok_error
-
-$screen_cmd "grub-set-default 1 ${assess_update_errors}"
-grok_error
-
-$screen_cmd "update-grub ${assess_update_errors}"
-grok_error
-
-$screen_cmd "sed -i 's/timeout 5/timeout 9/g' /boot/grub/menu.lst ${assess_update_errors}"
-grok_error
-
-$screen_cmd "sed -i 's/.*kopt=root=.*/# kopt=root=/dev/xvda console=hvc0 ro quiet/g' /boot/grub/menu.lst ${assess_update_errors}"
-grok_error
-
-$screen_cmd "sed -i 's/.*groot=.*/# groot=(hd0)/g' /boot/grub/menu.lst ${assess_update_errors}"
-grok_error
-
-$screen_cmd "update-grub ${assess_update_errors}"
-grok_error
+sudo /bin/bash --login -c "mv ${system_map} /boot && chmod 0644 ${system_map} && chown root:root ${system_map}"
+sudo /bin/bash --login -c "mv ${config} /boot && chmod 0644 ${config} && chown root:root ${config}"
+sudo /bin/bash --login -c "mv ${initrd} /boot && chmod 0644 ${initrd} && chown root:root ${initrd}"
+sudo /bin/bash --login -c "mv ${kernel} /boot && chmod 0644 ${kernel} && chown root:root ${kernel}"
