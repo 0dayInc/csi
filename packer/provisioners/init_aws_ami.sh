890159c8998affb564e78b5baf30cc7b94d7c285
Jacob Hoopes <jake.hoopes@gmail.com>
2019-09-04 13:59:34 -0600

    Packer - init_aws_ami bugfix in deb-src provisioner

--- packer/provisioners/init_aws_ami.sh
+++ packer/provisioners/init_aws_ami.sh
@@ -1,7 +1,7 @@
 #!/bin/bash --login
 # Begin Converting to Kali Rolling
 sudo /bin/bash --login -c "DEBIAN_FRONTEND=noninteractive apt install -yq dirmngr software-properties-common"
-sudo /bin/bash --login -c "> /etc/apt/sources.list && add-apt-repository 'deb https://http.kali.org/kali kali-rolling main contrib non-free' && add-apt-repository 'deb-src https://http.kali.org/kali kali-rolling main contrib non-free'"
+sudo /bin/bash --login -c "> /etc/apt/sources.list && add-apt-repository 'deb https://http.kali.org/kali kali-rolling main non-free contrib' && echo 'deb-src https://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list"
 
 # Download and import the official Kali Linux key
 wget -q -O - https://www.kali.org/archive-key.asc | sudo apt-key add -
