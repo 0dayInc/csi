0abdf86f3390d080624161fd9b54140fff4d5265
Jacob Hoopes <jake.hoopes@gmail.com>
2019-09-05 15:55:50 -0600

    Packer apache provisioner - bugfix in userland path

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.738'
+  VERSION = '0.3.739'
 end
--- packer/provisioners/apache2.sh
+++ packer/provisioners/apache2.sh
@@ -1,4 +1,6 @@
 #!/bin/bash --login
+csi_provider=`echo $CSI_PROVIDER`
+
 sudo apt install -y apache2
 sudo a2enmod proxy
 sudo a2enmod proxy_http
@@ -7,4 +9,4 @@ sudo a2enmod ssl
 sudo a2enmod headers
 # Disable Version Headers
 sudo /bin/bash --login -c 'echo -e "ServerSignature Off\nServerTokens Prod" >> /etc/apache2/apache2.conf'
-sudo /bin/bash --login -c "cp /csi/etc/apache2/*.conf /etc/apache2/sites-available/"
+sudo /bin/bash --login -c "cp /csi/userland/${csi_provider}/etc/apache2/*.conf /etc/apache2/sites-available/"
