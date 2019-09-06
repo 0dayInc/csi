5be77f63f822d85571da6b7767c3fc1c38528e68
Jacob Hoopes <jake.hoopes@gmail.com>
2019-09-06 08:44:49 -0600

    Packer post_install provisioner - cleanup logs

--- packer/provisioners/post_install.sh
+++ packer/provisioners/post_install.sh
@@ -5,6 +5,16 @@ csi_provider=`echo $CSI_PROVIDER`
 sudo /bin/bash --login -c 'find /home -type f -name ".*history" -exec shred -u {} \;'
 sudo /bin/bash --login -c 'find /root -type f -name ".*history" -exec shred -u {} \;'
 
+# Cleanup Logs
+sudo /bin/bash --login -c 'find /var/log -type f -name "*.log" -exec > {} \;'
+sudo /bin/bash --login -c 'find /var/log -type f -name "dmesg.*" -exec rm {} \;'
+sudo /bin/bash --login -c '> /var/log/debug'
+sudo /bin/bash --login -c '> /var/log/dmesg'
+sudo /bin/bash --login -c '> /var/log/lastlog'
+sudo /bin/bash --login -c '> /var/log/messages'
+sudo /bin/bash --login -c '> /var/log/syslog'
+sudo /bin/bash --login -c '> /var/log/wtmp'
+
 # Disable Local Root Access
 sudo passwd -l root
 
@@ -14,6 +24,10 @@ sudo shred -u /etc/ssh/*_key /etc/ssh/*_key.pub
 if [[ $csi_provider == 'aws' ]]; then
   sudo /bin/bash --login -c 'find /home -type d -name "authorized_keys" -exec shred -u {} \;'
 fi
+
+# Clear Bash History
+sudo history -c
+history -c
 
 if [[ $csi_provider != 'aws' ]]; then
   # Remove csiadmin account if it exists
