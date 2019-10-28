c2a0cbc581e6ba60da5a78e99b4b9b55ee0ccb5d
Jacob Hoopes <jake.hoopes@gmail.com>
2019-10-28 12:16:08 -0600

    Vagrantfile - remove volume mount for docker containers for now

--- packer/provisioners/install_vagrant_ssh_key.sh
+++ packer/provisioners/install_vagrant_ssh_key.sh
@@ -1,10 +1,12 @@
 #!/bin/bash --login
-if [[ ! -d '/home/admin/.ssh' ]]; then
-  sudo mkdir -p /home/admin/.ssh
-  sudo chmod 0700 /home/admin/.ssh
-  sudo touch /home/admin/.ssh/authorized_keys
-  sudo chmod 0600 /home/admin/.ssh/authorized_keys
-  sudo chown -R admin:admin /home/admin/.ssh
-fi
+for user in csiadmin admin; do
+  if [[ ! -d "/home/${user}/.ssh" ]]; then
+    sudo mkdir -p /home/$user/.ssh
+    sudo chmod 0700 /home/$user/.ssh
+    sudo touch /home/$user/.ssh/authorized_keys
+    sudo chmod 0600 /home/$user/.ssh/authorized_keys
+    sudo chown -R $user:$user /home/$user/.ssh
+  fi
 
-sudo /bin/bash --login -c 'wget --no-check-certificate https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub -O ->> /home/admin/.ssh/authorized_keys'
+  sudo /bin/bash --login -c "wget --no-check-certificate https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub -O ->> /home/${user}/.ssh/authorized_keys"
+done
