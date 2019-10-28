bf3b312acb0c03820f8eb7c2b9e26135de430909
Jacob Hoopes <jake.hoopes@gmail.com>
2019-10-28 16:03:02 -0600

    Vagrant post_install.sh - bugfix in post_install.sh SSH key generation

--- vagrant/provisioners/post_install.sh
+++ vagrant/provisioners/post_install.sh
@@ -5,8 +5,8 @@ sudo userdel -r csiadmin
 
 # Regenerate SSH Keys
 # RSA
-yes y | sudo ssh-keygen -f /ets/ssh/ssh_host_rsa_key -N '' -t rsa -b 8192
+yes y | sudo ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa -b 8192
 # DSA
-yes y | sudo ssh-keygen -f /ets/ssh/ssh_host_dsa_key -N '' -t dsa -b 1024
+yes y | sudo ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa -b 1024
 # ECDSA
-yes y | sudo ssh-keygen -f /ets/ssh/ssh_host_ecdsa_key -N '' -t ecdsa -b 521
+yes y | sudo ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa -b 521
