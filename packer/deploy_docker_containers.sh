0bde5f037293181c006f0217b5e6cc291806591d
Jacob Hoopes <jake.hoopes@gmail.com>
2019-10-31 16:48:48 -0600

    Packer deploy_docker_containers.sh - add sleep prior to moving onto next

--- packer/deploy_docker_containers.sh
+++ packer/deploy_docker_containers.sh
@@ -3,4 +3,5 @@
   ./deploy_packer_box.sh $c latest
   docker images -a | grep -v csi_prototyper | awk '{print $3}' | while read i; do docker rmi --force $i; done
   docker system prune -f
+  sleep 9
 done
