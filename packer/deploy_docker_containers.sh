e1fed7b35f494c2a070909371163dc7b636bd8c0
Jacob Hoopes <jake.hoopes@gmail.com>
2019-11-01 09:44:42 -0600

    Packer deploy_docker_containers.sh - cleanup images following upload to DockerHub to reduce space needed on AWS

--- packer/deploy_docker_containers.sh
+++ packer/deploy_docker_containers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash --login
 cd $CSI_ROOT/packer && ./deploy_packer_box.sh | grep docker | awk '{print $1}' | while read c; do 
   ./deploy_packer_box.sh $c latest
-  docker images -a | grep -v csi_prototyper | awk '{print $3}' | while read i; do docker rmi --force $i; done
+  docker images -a | grep -v REPOSITORY | awk '{print $3}' | while read i; do docker rmi --force $i; done
   docker system prune -f
   sleep 9
 done
