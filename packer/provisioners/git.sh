67295e26b811a31714e15aa369fc4f61658618d5
Jacob Hoopes <jake.hoopes@gmail.com>
2020-03-11 09:32:57 -0600

    Packer - hard-coded CSI_ROOT into provisioners

--- packer/provisioners/git.sh
+++ packer/provisioners/git.sh
@@ -2,5 +2,6 @@
 source /etc/profile.d/globals.sh
 
 printf "Installing Git ********************************************************************"
-$screen_cmd "${apt} install -y git ${assess_update_errors}"
+#$screen_cmd "${apt} install -y git ${assess_update_errors}"
+$screen_cmd "${apt} install -y git"
 grok_error
