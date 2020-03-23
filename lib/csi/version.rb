01423d467d9862652c06fee6457a587bb529ffcc
Jacob Hoopes <jake.hoopes@gmail.com>
2020-03-23 15:43:41 -0600

    Gemfile - upgrade pry gem to support ruby-2.7 compatibility

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.894'
+  VERSION = '0.3.895'
 end
--- packer/provisioners/docker.sh
+++ packer/provisioners/docker.sh
@@ -3,38 +3,38 @@ source /etc/profile.d/globals.sh
 
 printf "Installing Docker ********************************************************************"
 docker_sources='/etc/apt/sources.list.d/docker.list'
-$screen_cmd "${apt} remove -y docker docker-engine docker.io* ${assess_update_errors}"
+$screen_cmd "${apt} remove -y docker docker-engine docker.io*"
 grok_error
 
-$screen_cmd "${apt} install -y apt-transport-https ${assess_update_errors}"
+$screen_cmd "${apt} install -y apt-transport-https"
 grok_error
 
-$screen_cmd "${apt} install -y ca-certificates ${assess_update_errors}"
+$screen_cmd "${apt} install -y ca-certificates"
 grok_error
 
-$screen_cmd "${apt} install -y curl ${assess_update_errors}"
+$screen_cmd "${apt} install -y curl"
 grok_error
 
-$screen_cmd "${apt} install -y gnupg2 ${assess_update_errors}"
+$screen_cmd "${apt} install -y gnupg2"
 grok_error
 
-$screen_cmd "${apt} install -y software-properties-common ${assess_update_errors}"
+$screen_cmd "${apt} install -y software-properties-common"
 grok_error
 
-$screen_cmd "curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - ${assess_update_errors}"
+$screen_cmd "curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -"
 grok_error
 
 $screen_cmd "echo 'deb [arch=amd64] https://download.docker.com/linux/debian stretch stable' > ${docker_sources}"
 grok_error
 
-$screen_cmd "${apt} update ${assess_update_errors}"
+$screen_cmd "${apt} update"
 grok_error
 
-$screen_cmd "${apt} install -y docker-ce docker-compose ${assess_update_errors}"
+$screen_cmd "${apt} install -y docker-ce docker-compose"
 grok_error
 
-$screen_cmd "usermod -aG docker vagrant ${assess_update_errors}"
+$screen_cmd "usermod -aG docker vagrant"
 grok_error
 
-$screen_cmd "systemctl enable docker ${assess_update_errors}"
+$screen_cmd "systemctl enable docker"
 grok_error
