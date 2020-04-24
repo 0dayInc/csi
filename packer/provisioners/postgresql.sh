112128f0c88fde844a146160f2b37d92ea81c3a9
Jacob Hoopes <jake.hoopes@gmail.com>
2020-04-24 14:55:42 -0600

    Packer - bugfix in postgresql provisioner

--- packer/provisioners/postgresql.sh
+++ packer/provisioners/postgresql.sh
@@ -11,21 +11,18 @@ grok_error
 $screen_cmd "sudo -iu postgres createdb ${pdb} ${assess_update_errors}"
 grok_error
 
-current_table=''
-csi_table_def=$(cat <<EOF
-  create table ${current_table} (
-    id SERIAL PRIMARY KEY,
-    row_result varchar(33000) NOT NULL,
-  );
+function create_table() {
+  current_table="${1}"
+  csi_table_def=$(cat <<EOF
+    create table ${current_table} (
+      id SERIAL PRIMARY KEY,
+      row_result varchar(33000) NOT NULL,
+    );
 EOF
-)
+  )
+  $screen_cmd "echo ${csi_table_def} | sudo -iu postgres psql -X -d ${pdb}"
+  grok_error
+}
 
-# csi_scapm
-current_table='scapm'
-$screen_cmd "echo ${csi_table_def} | sudo -iu postgres psql -X -d ${pdb}"
-grok_error
-
-# csi_fuzz_net_app_proto
-current_table='fuzz_net_app_proto'
-$screen_cmd "echo ${csi_table_def} | sudo -iu postgres psql -X -d ${pdb}"
-grok_error
+create_table 'scapm'
+create_table 'fuzz_net_app_proto'
