9c6105474909299e16bd16289c3731179d814e7d
Jacob Hoopes <jake.hoopes@gmail.com>
2020-06-24 14:36:23 -0600

    FUNDING.yml - Initial commit for sponsorship

--- lib/csi/plugins/dao_sqlite3.rb
+++ lib/csi/plugins/dao_sqlite3.rb
@@ -75,7 +75,7 @@ module CSI
             res = sqlite3_conn.execute(prepared_statement, statement_params)
           end
         rescue SQLite3::BusyException
-          puts 'Database In Use...Sleeping.'
+          puts 'Database In Use - Retrying...'
           retry
         end
 
