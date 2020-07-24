d29774f89d024e343286205d516334f6697cadff
Jacob Hoopes <jake.hoopes@gmail.com>
2020-07-24 11:31:47 -0600

    Packer - update afl provisioner to include dependencies for qemu instrmentation #remove_apt_install_qemu

--- packer/provisioners/afl.sh
+++ packer/provisioners/afl.sh
@@ -3,7 +3,9 @@ source /etc/profile.d/globals.sh
 
 # Build from source in order to support Qemu Instrumentation:
 # Found in https://github.com/mirrorer/afl/README.md => "4) Instrumenting binary-only apps"
-$screen_cmd "${apt} install -y libtool libtool-bin automake bison libglib2.0-dev qemu"
+# No need to apt install qemu as it's included in:
+# /opt/afl-dev/qemu_mode/qemu-2.10.0.tar.xz
+$screen_cmd "${apt} install -y libtool libtool-bin automake bison libglib2.0-dev"
 grok_error
 
 $screen_cmd "cd /opt && git clone https://github.com/mirrorer/afl afl-dev && cd /opt/afl-dev && make && cd /opt/afl-dev/qemu_mode && ./build_qemu_support.sh"
