62ac84320fa6ac0a5e0f5710972bb5e1f74ee2f2
Jacob Hoopes <jake.hoopes@gmail.com>
2020-07-29 09:33:25 -0600

    Gemfile - include savon gem for SOAP testing

--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.3.989'
+  VERSION = '0.3.990'
 end
--- packer/provisioners/afl.sh
+++ packer/provisioners/afl.sh
@@ -5,10 +5,10 @@ source /etc/profile.d/globals.sh
 # Found in https://github.com/mirrorer/afl/README.md => "4) Instrumenting binary-only apps"
 # No need to apt install qemu as it's included in:
 # /opt/afl-dev/qemu_mode/qemu-2.10.0.tar.xz
-$screen_cmd "${apt} install -y libtool libtool-bin automake bison libglib2.0-dev"
+$screen_cmd "${apt} install -y libtool libtool-bin automake bison libglib2.0-dev ${assess_update_errors}"
 grok_error
 
-$screen_cmd "cd /opt && git clone https://github.com/mirrorer/afl afl-dev && cd /opt/afl-dev && make && cd /opt/afl-dev/qemu_mode && ./build_qemu_support.sh"
+$screen_cmd "cd /opt && git clone https://github.com/mirrorer/afl afl-dev && cd /opt/afl-dev && make && cd /opt/afl-dev/qemu_mode && ./build_qemu_support.sh ${assess_update_errors}"
 grok_error
 
 ls -l /opt/afl-dev | grep '^-rwx' | awk '{print $9}' | while read afl_bin; do 
