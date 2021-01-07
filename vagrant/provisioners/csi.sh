48b4d5147b3355a27bd12dbd2df7ba45edfc43fa
Jacob Hoopes <jake.hoopes@gmail.com>
2021-01-07 10:48:27 -0700

    Begin conforming to RVM best practices when building gems

--- vagrant/provisioners/csi.sh
+++ vagrant/provisioners/csi.sh
@@ -9,5 +9,7 @@ else
   csi_root="${CSI_ROOT}"
 fi
 
-sudo /bin/bash --login -c "cd ${csi_root} && ./reinstall_csi_gemset.sh"
-sudo /bin/bash --login -c "cd ${csi_root} && ./build_csi_gem.sh"
+#sudo /bin/bash --login -c "cd ${csi_root} && ./reinstall_csi_gemset.sh"
+#sudo /bin/bash --login -c "cd ${csi_root} && ./build_csi_gem.sh"
+rvmsudo /bin/bash --login -c "cd ${csi_root} && ./reinstall_csi_gemset.sh"
+rvmsudo /bin/bash --login -c "cd ${csi_root} && ./build_csi_gem.sh"
