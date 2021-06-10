74f5a4f806c3453ef11fbf13742dfb49e6eea723
Jacob Hoopes <jake.hoopes@gmail.com>
2021-06-10 14:42:45 -0600

    Gemfile - Begin transitioning to Watir 7.x

--- find_latest_gem_versions_per_Gemfile.sh
+++ find_latest_gem_versions_per_Gemfile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash --login
-cat Gemfile | awk '{print $2}' | grep -E "^'.+$" | grep -v rubygems.org | while read gem; do 
+# Uncomment when watir 7 is released.
+# cat Gemfile | awk '{print $2}' | grep -E "^'.+$" | grep -v rubygems.org | while read gem; do 
+cat Gemfile | awk '{print $2}' | grep -E "^'.+$" | grep -v rubygems.org | grep -v watir | while read gem; do 
   this_gem=`echo $gem | sed "s/'//g" | sed 's/\,//g'`
   latest_version=`gem search -r $this_gem | grep -E "^${this_gem}\s.+$" | awk '{print $2}' | sed 's/(//g' | sed 's/)//g' | sed 's/,//g'`
   echo "${this_gem} => $latest_version"
--- lib/csi/version.rb
+++ lib/csi/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module CSI
-  VERSION = '0.4.83'
+  VERSION = '0.4.84'
 end
